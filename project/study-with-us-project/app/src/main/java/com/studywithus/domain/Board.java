c7944dafd784c1ddb9f087efb3f3886f456f1ba0
SeonyoungHa <syh5004@naver.com>
2021-08-26 16:32:22 +0900

    upload-ChargeStudyHandler

--- project/study-with-us-project/app/src/main/java/com/studywithus/domain/Board.java
+++ project/study-with-us-project/app/src/main/java/com/studywithus/domain/Board.java
@@ -1,12 +1,17 @@
 package com.studywithus.domain;
 
+import java.sql.Date;
+
 public class Board {
+
 	public int no;
+	public String title;
 	public String name;
 	public String content;
 	public String writer;
-	public String registeredDate;
+	public Date registeredDate;
 	public int like;
 	public int viewCount;
+
 }
 
--- project/study-with-us-project/app/src/main/java/com/studywithus/domain/Charge.java
+++
@@ -1,5 +0,0 @@
-package com.studywithus.domain;
-
-public class Charge {
-
-}
