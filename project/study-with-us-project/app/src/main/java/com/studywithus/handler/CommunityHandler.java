0eccd545115afde589030b2cd5510e1ffdc82947
Gyeryeong Ban <gyeryeong.ban@outlook.com>
2021-08-26 21:00:53 +0900

    Update File

--- project/study-with-us-project/app/src/main/java/com/studywithus/handler/CommunityHandler.java
+++ project/study-with-us-project/app/src/main/java/com/studywithus/handler/CommunityHandler.java
@@ -30,8 +30,9 @@ public class CommunityHandler {
   public void list() {
     System.out.println("[커뮤니티 조회]");
     for (int i = 0; i < this.size; i++) {
-      System.out.printf("%d, %s, %s, %s, %d, %d\n", this.communities[i].no, this.communities[i].title, this.communities[i].writer,
-          this.communities[i].registeredDate, this.communities[i].viewCount, this.communities[i].like);
+      System.out.printf("%d, %s, %s, %s, %d, %d\n", this.communities[i].no, this.communities[i].title,
+          this.communities[i].writer, this.communities[i].registeredDate, this.communities[i].viewCount,
+          this.communities[i].like);
     }
   }
 
@@ -40,18 +41,18 @@ public class CommunityHandler {
     System.out.println("[커뮤니티 상세보기]");
     int no = Prompt.inputInt("번호? ");
 
-    Board board = findByNo(no);
+    Board community = findByNo(no);
 
-    if (board == null) {
+    if (community == null) {
       System.out.println("해당 번호의 커뮤니티 게시글이 없습니다.");
       return;
     }
 
-    System.out.printf("제목: %s\n", board.title);
-    System.out.printf("내용: %s\n", board.content);
-    System.out.printf("작성자: %s\n", board.writer);
-    System.out.printf("등록일: %s\n", board.registeredDate);
-    System.out.printf("조회수: %d\n", ++board.viewCount);
+    System.out.printf("제목: %s\n", community.title);
+    System.out.printf("내용: %s\n", community.content);
+    System.out.printf("작성자: %s\n", community.writer);
+    System.out.printf("등록일: %s\n", community.registeredDate);
+    System.out.printf("조회수: %d\n", ++community.viewCount);
   }
 
   // 커뮤니티 변경
@@ -59,15 +60,15 @@ public class CommunityHandler {
     System.out.println("[커뮤니티 변경]");
     int no = Prompt.inputInt("번호? ");
 
-    Board board = findByNo(no);
+    Board community = findByNo(no);
 
-    if (board == null) {
+    if (community == null) {
       System.out.println("해당 번호의 커뮤니티 게시글이 없습니다.");
       return;
     }
 
-    String title = Prompt.inputString(String.format("[%s] 수정된 제목: ", board.title));
-    String content = Prompt.inputString(String.format("[%s] 수정된 내용: ", board.content));
+    String title = Prompt.inputString(String.format("[%s] 수정된 제목: ", community.title));
+    String content = Prompt.inputString(String.format("[%s] 수정된 내용: ", community.content));
 
     String input = Prompt.inputString("정말 변경하시겠습니까? (y/N) ");
     if (input.equalsIgnoreCase("n") || input.length() == 0) {
@@ -75,8 +76,8 @@ public class CommunityHandler {
       return;
     }
 
-    board.title = title;
-    board.content = content;
+    community.title = title;
+    community.content = content;
     System.out.println("커뮤니티 게시글을 변경하였습니다.");
   }
 
