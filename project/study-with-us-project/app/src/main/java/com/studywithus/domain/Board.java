fcd2b3cbf127b5712b073995a439e0c9cbf0ff11
SeonyoungHa <syh5004@naver.com>
2021-08-26 16:23:51 +0900

    upload-Board

--- project/study-with-us-project/app/src/main/java/com/studywithus/domain/Board.java
+++ project/study-with-us-project/app/src/main/java/com/studywithus/domain/Board.java
@@ -1,5 +1,12 @@
 package com.studywithus.domain;
 
 public class Board {
-
+	public int no;
+	public String name;
+	public String content;
+	public String writer;
+	public String registeredDate;
+	public int like;
+	public int viewCount;
 }
+
--- project/study-with-us-project/app/src/main/java/com/studywithus/domain/Charge.java
+++ project/study-with-us-project/app/src/main/java/com/studywithus/domain/Charge.java
@@ -1,12 +1,5 @@
 package com.studywithus.domain;
 
-// 유료스터디
 public class Charge {
-	public int no;
-	public String name;
-	public String content;
-	public String writer;
-	public String registeredDate;
-	public int like;
-	public int viewCount;
+
 }
