eed1537c725a4434cec7700d731cf6f43104c609
Gyeryeong Ban <gyeryeong.ban@outlook.com>
2021-08-26 19:56:16 +0900

    Update File

--- project/study-with-us-project/app/src/main/java/com/studywithus/App.java
+++ project/study-with-us-project/app/src/main/java/com/studywithus/App.java
@@ -3,96 +3,92 @@
  */
 package com.studywithus;
 
-import com.studywithus.handler.BoardHandler;
 import com.studywithus.handler.ChargeStudyHandler;
 import com.studywithus.handler.FreeStudyHandler;
 import com.studywithus.util.Prompt;
 
 public class App {
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 
-		BoardHandler boardHandler = new BoardHandler();
-		ChargeStudyHandler chargeStudyHandler = new ChargeStudyHandler();
-		FreeStudyHandler freeStudyHandler = new FreeStudyHandler();
-		ProjectHandler projectHandler = new ProjectHandler(memberHandler);
-		TaskHandler taskHandler = new TaskHandler(memberHandler);
+    ChargeStudyHandler chargeStudyHandler = new ChargeStudyHandler();
+    FreeStudyHandler freeStudyHandler = new FreeStudyHandler();
 
-		while (true) {
-			String input = Prompt.inputString("명령> ");
+    while (true) {
+      String input = Prompt.inputString("명령> ");
 
-			if (input.equals("exit") || input.equals("quit")) {
-				System.out.println("안녕!");
-				break;
-			} else if (input.equals("/member/add")) {
-				memberHandler.add();
+      if (input.equals("exit") || input.equals("quit")) {
+        System.out.println("안녕!");
+        break;
+      } else if (input.equals("/member/add")) {
+        memberHandler.add();
 
-			} else if (input.equals("/member/list")) {
-				memberHandler.list();
+      } else if (input.equals("/member/list")) {
+        memberHandler.list();
 
-			} else if (input.equals("/member/detail")) {
-				memberHandler.detail();
+      } else if (input.equals("/member/detail")) {
+        memberHandler.detail();
 
-			} else if (input.equals("/member/update")) {
-				memberHandler.update();
+      } else if (input.equals("/member/update")) {
+        memberHandler.update();
 
-			} else if (input.equals("/member/delete")) {
-				memberHandler.delete();
+      } else if (input.equals("/member/delete")) {
+        memberHandler.delete();
 
-			}  else if (input.equals("/project/add")) {
-				projectHandler.add();
+      }  else if (input.equals("/project/add")) {
+        projectHandler.add();
 
-			}  else if (input.equals("/project/list")) {
-				projectHandler.list();
+      }  else if (input.equals("/project/list")) {
+        projectHandler.list();
 
-			}  else if (input.equals("/project/detail")) {
-				projectHandler.detail();
+      }  else if (input.equals("/project/detail")) {
+        projectHandler.detail();
 
-			}  else if (input.equals("/project/update")) {
-				projectHandler.update();
+      }  else if (input.equals("/project/update")) {
+        projectHandler.update();
 
-			}  else if (input.equals("/project/delete")) {
-				projectHandler.delete();
+      }  else if (input.equals("/project/delete")) {
+        projectHandler.delete();
 
-			}  else if (input.equals("/task/add")) {
-				taskHandler.add();
+      }  else if (input.equals("/task/add")) {
+        taskHandler.add();
 
-			}  else if (input.equals("/task/list")) {
-				taskHandler.list();
+      }  else if (input.equals("/task/list")) {
+        taskHandler.list();
 
-			}  else if (input.equals("/task/detail")) {
-				taskHandler.detail();
+      }  else if (input.equals("/task/detail")) {
+        taskHandler.detail();
 
-			}  else if (input.equals("/task/update")) {
-				taskHandler.update();
+      }  else if (input.equals("/task/update")) {
+        taskHandler.update();
 
-			}  else if (input.equals("/task/delete")) {
-				taskHandler.delete();
+      }  else if (input.equals("/task/delete")) {
+        taskHandler.delete();
 
-			}  else if (input.equals("/board/add")) {
-				boardHandler.add();
+      }  else if (input.equals("/board/add")) {
+        boardHandler.add();
 
-			}  else if (input.equals("/board/list")) {
-				boardHandler.list();
+      }  else if (input.equals("/board/list")) {
+        boardHandler.list();
 
-			}  else if (input.equals("/board/detail")) {
-				boardHandler.detail();
+      }  else if (input.equals("/board/detail")) {
+        boardHandler.detail();
 
-			}  else if (input.equals("/board/update")) {
-				boardHandler.update();
+      }  else if (input.equals("/board/update")) {
+        boardHandler.update();
 
-			}  else if (input.equals("/board/delete")) {
-				boardHandler.delete();
+      }  else if (input.equals("/board/delete")) {
+        boardHandler.delete();
 
-			} else {
-				System.out.println("실행할 수 없는 명령입니다.");
-			}
+      } else {
+        System.out.println("실행할 수 없는 명령입니다.");
+      }
 
-			System.out.println();
-		}
+      System.out.println();
+    }
 
-		Prompt.close();
-	}
+    Prompt.close();
+  }
 }
 
 
