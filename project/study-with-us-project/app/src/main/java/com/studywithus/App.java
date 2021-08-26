2d74a88c25b93d0689c2712cb0cfbd9fc6837cf7
SeonyoungHa <syh5004@naver.com>
2021-08-26 17:18:49 +0900

    upload-file

--- project/study-with-us-project/app/src/main/java/com/studywithus/App.java
+++ project/study-with-us-project/app/src/main/java/com/studywithus/App.java
@@ -3,12 +3,134 @@
  */
 package com.studywithus;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import com.studywithus.handler.BoardHandler;
+import com.studywithus.handler.ChargeStudyHandler;
+import com.studywithus.handler.FreeStudyHandler;
+import com.studywithus.util.Prompt;
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-    }
+public class App {
+
+	public static void main(String[] args) {
+
+		BoardHandler boardHandler = new BoardHandler();
+		ChargeStudyHandler chargeStudyHandler = new ChargeStudyHandler();
+		FreeStudyHandler freeStudyHandler = new FreeStudyHandler();
+
+		// 만약 ProjectHandler의 메서드가 사용할 의존 객체를 주입하지 않는다면?
+		// - 그 의존 객체를 사용하는 메서드를 호출할 때 실행 오류가 발생할 것이다.
+		//    ProjectHandler projectHandler = new ProjectHandler();
+		//    projectHandler.memberHandler = memberHandler;
+		//
+		// 왜 이런 문제가 발생하는가?
+		// - 의존 객체 주입을 강제하지 않기 때문이다.
+		// 해결책?
+		// - 의존 객체 주입을 강제하면 된다.
+		// - ProjectHandler의 인스턴스를 생성할 때 반드시 MemberHandler의 인스턴스를 주입하게 
+		//   만들면 된다.
+		// 어떻게?
+		// - 생성자를 도입하라!
+		ProjectHandler projectHandler = new ProjectHandler(memberHandler);
+
+		// 생성자를 이용하면 다음과 같이 인스턴스를 생성할 때 의존 객체 주입을 강제할 수 있다.
+		TaskHandler taskHandler = new TaskHandler(memberHandler);
+
+		while (true) {
+			String input = Prompt.inputString("명령> ");
+
+			if (input.equals("exit") || input.equals("quit")) {
+				System.out.println("안녕!");
+				break;
+			} else if (input.equals("/member/add")) {
+				memberHandler.add();
+
+			} else if (input.equals("/member/list")) {
+				memberHandler.list();
+
+			} else if (input.equals("/member/detail")) {
+				memberHandler.detail();
+
+			} else if (input.equals("/member/update")) {
+				memberHandler.update();
+
+			} else if (input.equals("/member/delete")) {
+				memberHandler.delete();
+
+			}  else if (input.equals("/project/add")) {
+				// add() 메서드가 사용할 의존 객체를 미리 주입했기 때문에
+				// 이제 파라미터로 전달할 필요가 없다.
+				//        projectHandler.add(memberHandler); // 이전 코드 
+				projectHandler.add();
+
+			}  else if (input.equals("/project/list")) {
+				projectHandler.list();
+
+			}  else if (input.equals("/project/detail")) {
+				projectHandler.detail();
+
+			}  else if (input.equals("/project/update")) {
+				// update() 메서드가 사용할 의존 객체를 미리 주입했기 때문에
+				// 이제 파라미터로 전달할 필요가 없다.
+				//        projectHandler.update(memberHandler); // 이전 코드
+				projectHandler.update();
+
+			}  else if (input.equals("/project/delete")) {
+				projectHandler.delete();
+
+			}  else if (input.equals("/task/add")) {
+				// add() 메서드가 사용할 의존 객체를 미리 주입했기 때문에
+				// 이제 파라미터로 전달할 필요가 없다.
+				//        taskHandler.add(memberHandler); // 이전 코드
+				taskHandler.add();
+
+			}  else if (input.equals("/task/list")) {
+				taskHandler.list();
+
+			}  else if (input.equals("/task/detail")) {
+				taskHandler.detail();
+
+			}  else if (input.equals("/task/update")) {
+				// update() 메서드가 사용할 의존 객체를 미리 주입했기 때문에
+				// 이제 파라미터로 전달할 필요가 없다.
+				//        taskHandler.update(memberHandler); // 이전 코드
+				taskHandler.update();
+
+			}  else if (input.equals("/task/delete")) {
+				taskHandler.delete();
+
+			}  else if (input.equals("/board/add")) {
+				boardHandler.add();
+
+			}  else if (input.equals("/board/list")) {
+				boardHandler.list();
+
+			}  else if (input.equals("/board/detail")) {
+				boardHandler.detail();
+
+			}  else if (input.equals("/board/update")) {
+				boardHandler.update();
+
+			}  else if (input.equals("/board/delete")) {
+				boardHandler.delete();
+
+			} else {
+				System.out.println("실행할 수 없는 명령입니다.");
+			}
+			System.out.println();
+		}
+
+		// Prompt 가 소유하고 관리하고 있는 자원을 닫으라고 명령한다. 
+		Prompt.close();
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
--- project/study-with-us-project/app/src/main/java/com/studywithus/handler/ChargeStudyHandler.java
+++ project/study-with-us-project/app/src/main/java/com/studywithus/handler/ChargeStudyHandler.java
@@ -12,117 +12,136 @@ public class ChargeStudyHandler {
 
 	// 게시판 마다 따로 관리해야 하기 때문에 인스턴스 필드로 전환한다.
 	// => static 옵션을 뺀다.
-	Board[] boards = new Board[MAX_LENGTH];
+	Board[] studys = new Board[MAX_LENGTH];
 	int size = 0;
 
+	// 유료스터디 생성
 	public void add() {
-		System.out.println("[새 게시글]");
+		System.out.println("[새 유료스터디]");
 
-		Board board = new Board();
+		Board study = new Board();
 
-		board.no = Prompt.inputInt("번호? ");
-		board.title = Prompt.inputString("제목? ");
-		board.content = Prompt.inputString("내용? ");
-		board.writer = Prompt.inputString("작성자? ");
-		board.registeredDate = new Date(System.currentTimeMillis());
+		study.no = Prompt.inputInt("번호? ");
+		study.title = Prompt.inputString("제목? ");
+		study.content = Prompt.inputString("내용? ");
+		study.writer = Prompt.inputString("멘토? ");
+		study.registeredDate = new Date(System.currentTimeMillis());
 		//    board.viewCount = 0; // 인스턴스 변수는 생성되는 순간 기본 값이 0으로 설정된다.
 
-		this.boards[this.size++] = board;
+		this.studys[this.size++] = study;
 	}
 
+	// 유료스터디 목록
 	public void list() {
-		System.out.println("[게시글 목록]");
+		System.out.println("[유료스터디 목록]");
 		for (int i = 0; i < this.size; i++) {
 			System.out.printf("%d, %s, %s, %s, %d, %d\n", 
-					this.boards[i].no, 
-					this.boards[i].title, 
-					this.boards[i].writer,
-					this.boards[i].registeredDate,
-					this.boards[i].viewCount, 
-					this.boards[i].like);
+					this.studys[i].no, 
+					this.studys[i].title, 
+					this.studys[i].writer,
+					this.studys[i].registeredDate,
+					this.studys[i].viewCount, 
+					this.studys[i].like);
 		}
 	}
 
+	// 유료스터디 상세보기
 	public void detail() {
-		System.out.println("[게시글 상세보기]");
+		System.out.println("[유료스터디 상세보기]");
 		int no = Prompt.inputInt("번호? ");
 
-		Board board = findByNo(no);
+		Board study = findByNo(no);
 
-		if (board == null) {
+		if (study == null) {
 			System.out.println("해당 번호의 게시글이 없습니다.");
 			return;
 		}
 
-		System.out.printf("제목: %s\n", board.title);
-		System.out.printf("내용: %s\n", board.content);
-		System.out.printf("작성자: %s\n", board.writer);
-		System.out.printf("등록일: %s\n", board.registeredDate);
-		System.out.printf("조회수: %d\n", ++board.viewCount);
+		System.out.printf("제목: %s\n", study.title);
+		System.out.printf("내용: %s\n", study.content);
+		System.out.printf("작성자: %s\n", study.writer);
+		System.out.printf("등록일: %s\n", study.registeredDate);
+		System.out.printf("조회수: %d\n", ++study.viewCount);
 	}
 
+	// 유료스터디 변경
 	public void update() {
-		System.out.println("[게시글 변경]");
+		System.out.println("[유료스터디 변경]");
 		int no = Prompt.inputInt("번호? ");
 
-		Board board = findByNo(no);
+		Board study = findByNo(no);
 
-		if (board == null) {
-			System.out.println("해당 번호의 게시글이 없습니다.");
+		if (study == null) {
+			System.out.println("해당 번호의 유료스터디가 없습니다.");
 			return;
 		}
 
-		String title = Prompt.inputString(String.format("제목(%s)? ", board.title));
-		String content = Prompt.inputString(String.format("내용(%s)? ", board.content));
+		String title = Prompt.inputString(String.format("제목(%s)? ", study.title));
+		String content = Prompt.inputString(String.format("내용(%s)? ", study.content));
 
 		String input = Prompt.inputString("정말 변경하시겠습니까?(y/N) ");
 		if (input.equalsIgnoreCase("n") || input.length() == 0) {
-			System.out.println("게시글 변경을 취소하였습니다.");
+			System.out.println("유료스터디 변경을 취소하였습니다.");
 			return;
 		}
 
-		board.title = title;
-		board.content = content;
-		System.out.println("게시글을 변경하였습니다.");
+		study.title = title;
+		study.content = content;
+		System.out.println("유료스터디 변경하였습니다.");
 	}
 
+	// 유료스터디 삭제요청
 	public void delete() {
-		System.out.println("[게시글 삭제]");
+		System.out.println("[유료스터디 삭제]");
 		int no = Prompt.inputInt("번호? ");
 
 		int index = indexOf(no);
 
 		if (index == -1) {
-			System.out.println("해당 번호의 게시글이 없습니다.");
+			System.out.println("해당 번호의 유료스터디가 없습니다.");
 			return;
 		}
 
 		String input = Prompt.inputString("정말 삭제하시겠습니까?(y/N) ");
 		if (input.equalsIgnoreCase("n") || input.length() == 0) {
-			System.out.println("게시글 삭제를 취소하였습니다.");
+			System.out.println("유료스터디 삭제를 요청 하셨습니다.");
+			return;
+		}
+	}
+
+	// 삭제요청된 유료스터디 조회
+	public void deletedDetail() {
+		System.out.println("[삭제요청된 유료스터디 상세보기]");
+		int no = Prompt.inputInt("번호? ");
+
+		Board study = findByNo(no);
+
+		if (study == null) {
+			System.out.println("해당 번호의 삭제요청된 유료스터디가 없습니다.");
 			return;
 		}
 
-		for (int i = index + 1; i < this.size; i++) {
-			this.boards[i - 1] = this.boards[i];
-		}
-		this.boards[--this.size] = null;
-
-		System.out.println("게시글을 삭제하였습니다.");
+		System.out.printf("제목: %s\n", study.title);
+		System.out.printf("내용: %s\n", study.content);
+		System.out.printf("작성자: %s\n", study.writer);
+		System.out.printf("등록일: %s\n", study.registeredDate);
+		System.out.printf("조회수: %d\n", ++study.viewCount);
 	}
 
+	// 유료스터디 조회 (변경)
 	private Board findByNo(int no) {
 		for (int i = 0; i < this.size; i++) {
-			if (this.boards[i].no == no) {
-				return this.boards[i];
+			if (this.studys[i].no == no) {
+				return this.studys[i];
 			}
 		}
 		return null;
 	}
 
+	// 유료스터디 조회 (삭제)
 	private int indexOf(int no) {
 		for (int i = 0; i < this.size; i++) {
-			if (this.boards[i].no == no) {
+			if (this.studys[i].no == no) {
 				return i;
 			}
 		}
