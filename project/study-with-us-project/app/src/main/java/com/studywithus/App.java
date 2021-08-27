70a159774c99e7e5e6d695f45e57ec4379fc12e1
Gyeryeong Ban <gyeryeong.ban@outlook.com>
2021-08-27 22:59:17 +0900

    Update App

--- project/study-with-us-project/app/src/main/java/com/studywithus/App.java
+++ project/study-with-us-project/app/src/main/java/com/studywithus/App.java
@@ -1,15 +1,18 @@
 package com.studywithus;
+
 import com.studywithus.handler.ChargeStudyHandler;
 import com.studywithus.handler.CommunityHandler;
 import com.studywithus.handler.FreeStudyHandler;
 import com.studywithus.handler.NewMemberHandler;
 import com.studywithus.util.Prompt;
+
 public class App {
   public static void main(String[] args) {
     FreeStudyHandler freeStudyHandler = new FreeStudyHandler();
     CommunityHandler communityHandler = new CommunityHandler();
     NewMemberHandler newMemberHandler = new NewMemberHandler();
     ChargeStudyHandler chargeStudyHandler = new ChargeStudyHandler();
+    
     Main : while(true) {
       System.out.println();
       System.out.println("[STUDY WITH US]\n");
@@ -19,10 +22,12 @@ public class App {
       System.out.println("0. 종료하기\n");
       int input = Prompt.inputInt("메뉴를 선택해주세요. > ");
       System.out.println();
+      
       if(input==0) {
         System.out.println("종료되었습니다.");
         return;
       }
+
       NoneMember : while(true) {
         switch(input) {
           case 1 :
@@ -35,6 +40,7 @@ public class App {
             System.out.println("0. 이전\n");
             int input2 = Prompt.inputInt("메뉴를 선택해주세요. > ");
             System.out.println();
+            
             switch(input2) {
               case 1 : freeStudyHandler.list(); continue NoneMember;
               case 2 : chargeStudyHandler.list(); continue ;
@@ -43,16 +49,21 @@ public class App {
               default : System.out.println("잘못된 번호입니다.");
               continue NoneMember;
             }
+
           case 2 :
             Login : for(int i=0; i < newMemberHandler.newMembers.length;) {
               String id = Prompt.inputString("아이디 입력 :");
               String pwd = Prompt.inputString("비밀번호 입력 :");
+              
               if(!id.equals(newMemberHandler.newMembers[i].id) || !pwd.equals(newMemberHandler.newMembers[i].pwd)) {
                 System.out.println("아이디 또는 비밀번호가 다릅니다.\n");
                 continue Login;
-              }else{
+              }
+              
+              else{
                 System.out.println();
                 System.out.println("로그인 성공!!\n");
+                
                 Member : while(true) {
                   System.out.println("[회원 메뉴]\n");
                   System.out.println("1. 무료스터디");
@@ -62,10 +73,12 @@ public class App {
                   System.out.println("0. 로그아웃\n");
                   input = Prompt.inputInt("메뉴를 선택해주세요. > ");
                   System.out.println();
+                  
                   if(input==5) {
                     System.out.println("로그아웃 완료!");
                     continue Main;
                   }
+                  
                   Free : while(true) {
                     switch(input) {
                       case 1 :
@@ -77,6 +90,7 @@ public class App {
                         System.out.println("5. 삭제");
                         System.out.println("0. 이전\n");
                         int input3 = Prompt.inputInt("메뉴를 선택해주세요. > ");
+                        
                         switch(input3) {
                           case 1 : freeStudyHandler.add(); continue Free;
                           case 2 : freeStudyHandler.list(); continue Free;
@@ -87,6 +101,7 @@ public class App {
                           default : System.out.println("잘못된 번호입니다");
                           continue;
                         }
+
                       case 2 :
                         Charge : while(true) {
                           System.out.println("[유료 스터디]\n");
@@ -98,6 +113,7 @@ public class App {
                           System.out.println("0. 이전\n");
                           int input4 = Prompt.inputInt("메뉴를 선택해주세요. > ");
                           System.out.println();
+                          
                           switch(input4) {
                             case 1 : chargeStudyHandler.add(); continue Charge;
                             case 2 : chargeStudyHandler.list(); continue Charge;
@@ -109,12 +125,14 @@ public class App {
                             continue;
                           }
                         }
-                      case 3 :
+                      
+                        case 3 :
                         System.out.println("[관심목록]\n");
                         System.out.println("1. 조회");
                         System.out.println("2. 삭제");
                         System.out.println("아직 구현 못함ㅎㅎ");
-                      case 4 :
+                      
+                        case 4 :
                         Community : while(true) {
                           System.out.println("[커뮤니티]\n");
                           System.out.println("1. 생성");
@@ -125,6 +143,7 @@ public class App {
                           System.out.println("0. 이전");
                           int input5 = Prompt.inputInt("메뉴를 선택해주세요. > ");
                           System.out.println();
+                          
                           switch(input5) {
                             case 1 : communityHandler.add(); continue Community;
                             case 2 : communityHandler.list(); continue Community;
@@ -136,6 +155,7 @@ public class App {
                             continue;
                           }
                         }
+
                       case 0 : continue Main;
                       default : System.out.println("잘못된 번호입니다");
                       break;
@@ -145,11 +165,14 @@ public class App {
               }
             }
             break Main;
-          case 3 :
+          
+            case 3 :
             newMemberHandler.add();
             continue Main;
-          case 4 : System.out.println("종료되었습니다.");
+          
+            case 4 : System.out.println("종료되었습니다.");
           break Main;
+          
           default : System.out.println("잘못된 번호입니다");
           continue;
         }
