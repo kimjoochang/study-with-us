004526c333eb144d3737cc66a37db754d6ed6c34
kimjoochang <kimjc0707@naver.com>
2021-08-28 15:33:25 +0900

    Refactoring App

--- project/study-with-us-project/app/src/main/java/com/studywithus/domain/NewMember.java
+++ project/study-with-us-project/app/src/main/java/com/studywithus/domain/NewMember.java
@@ -1,6 +1,8 @@
 package com.studywithus.domain;
 
 public class NewMember {
+  public static String adminId = "admin";
+  public static String adminPwd = "admin";
   public String name;
   public String id;
   public String pwd;
