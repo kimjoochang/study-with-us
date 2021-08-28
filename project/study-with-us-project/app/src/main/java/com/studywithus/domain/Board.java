905d4b891fdad50a5807a417eb60fb47aa9ff779
SeonyoungHa <syh5004@naver.com>
2021-08-28 11:21:28 +0900

    Upload

--- project/study-with-us-project/app/src/main/java/com/studywithus/domain/Board.java
+++ project/study-with-us-project/app/src/main/java/com/studywithus/domain/Board.java
@@ -4,18 +4,22 @@ import java.sql.Date;
 
 public class Board {
 
-  public int no;
-  public String title;
-  public String name;
-  public String content;
-  public String writer;
-  public String onOffLine;
-  public String area;
-  public String explanation;
-  public String rule;
-  public Date registeredDate;
-  public int like;
-  public int viewCount;
+	public int no;
+	public String title;
+	public String name;
+	public String content;
+	public String writer;
+	public String onOffLine;
+	public String area;
+	public String explanation;
+	public String rule;
+	public Date registeredDate;
+	public int like;
+	public int viewCount;
+	public String mentorExplanation;
+	public String review;
+	public String payment;
+	public String termsCondition; //유료 약관동
 
 }
 
