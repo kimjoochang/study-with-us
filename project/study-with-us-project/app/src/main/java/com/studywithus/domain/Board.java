6ad25267bbf25fe1180b0f29b060361730051717
kimjoochang <kimjc0707@naver.com>
2021-08-31 00:20:50 +0900

    test

--- project/study-with-us-project/app/src/main/java/com/studywithus/domain/Board.java
+++ project/study-with-us-project/app/src/main/java/com/studywithus/domain/Board.java
@@ -1,18 +1,18 @@
 package com.studywithus.domain;
-
+//zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
 import java.sql.Date;
 
 public class Board {
 
-	public int no;
-	public String title;
-	public String name;
-	public String content;
-	public String writer;
-	public Date registeredDate;
-	public int like;
-	public int viewCount;
-	// 주석 
+  public int no;
+  public String title;
+  public String name;
+  public String content;
+  public String writer;
+  public Date registeredDate;
+  public int like;
+  public int viewCount;
+  // 주석 
 
 }
 
