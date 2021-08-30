9bb60de30f2d65b692dc0eeb9c9079520130a2c1
Gyeryeong Ban <gyeryeong.ban@outlook.com>
2021-08-30 19:33:26 +0900

    Update Getter/Setter

--- project/study-with-us-project/app/src/main/java/com/studywithus/domain/Calender.java
+++ project/study-with-us-project/app/src/main/java/com/studywithus/domain/Calender.java
@@ -1,18 +1,61 @@
 package com.studywithus.domain;
+
 import java.sql.Date;
-
-
 
 public class Calender {
 
+  private int no;
+  private String title;
+  private String content;
+  private Date startDate;
+  private Date endDate;
+  private String memo;
 
-  public int no;
-  public String title;
-  public String content;
-  public Date startDate;
-  public Date endDate;
-  public String memo;
+  public int getNo() {
+    return no;
+  }
 
+  public void setNo(int no) {
+    this.no = no;
+  }
 
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+  public Date getStartDate() {
+    return startDate;
+  }
+
+  public void setStartDate(Date startDate) {
+    this.startDate = startDate;
+  }
+
+  public Date getEndDate() {
+    return endDate;
+  }
+
+  public void setEndDate(Date endDate) {
+    this.endDate = endDate;
+  }
+
+  public String getMemo() {
+    return memo;
+  }
+
+  public void setMemo(String memo) {
+    this.memo = memo;
+  }
 }
-
--- project/study-with-us-project/app/src/main/java/com/studywithus/domain/FreeStudy.java
+++ project/study-with-us-project/app/src/main/java/com/studywithus/domain/FreeStudy.java
@@ -4,15 +4,94 @@ import java.sql.Date;
 
 public class FreeStudy {
 
-  public int no;
-  public String title;
-  public String writer;
-  public String onOffLine;
-  public String area;
-  public String explanation;
-  public String rule;
-  public Date registeredDate;
-  public int like;
-  public int viewCount;
+  private int no;
+  private String title;
+  private String writer;
+  private String onOffLine;
+  private String area;
+  private String explanation;
+  private String rule;
+  private Date registeredDate;
+  private int like;
+  private int viewCount;
 
+  public int getNo() {
+    return no;
+  }
+
+  public void setNo(int no) {
+    this.no = no;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getWriter() {
+    return writer;
+  }
+
+  public void setWriter(String writer) {
+    this.writer = writer;
+  }
+
+  public String getOnOffLine() {
+    return onOffLine;
+  }
+
+  public void setOnOffLine(String onOffLine) {
+    this.onOffLine = onOffLine;
+  }
+
+  public String getArea() {
+    return area;
+  }
+
+  public void setArea(String area) {
+    this.area = area;
+  }
+
+  public String getExplanation() {
+    return explanation;
+  }
+
+  public void setExplanation(String explanation) {
+    this.explanation = explanation;
+  }
+
+  public String getRule() {
+    return rule;
+  }
+
+  public void setRule(String rule) {
+    this.rule = rule;
+  }
+
+  public Date getRegisteredDate() {
+    return registeredDate;
+  }
+
+  public void setRegisteredDate(Date registeredDate) {
+    this.registeredDate = registeredDate;
+  }
+
+  public int getLike() {
+    return like;
+  }
+
+  public void setLike(int like) {
+    this.like = like;
+  }
+
+  public int getViewCount() {
+    return viewCount;
+  }
+
+  public void setViewCount(int viewCount) {
+    this.viewCount = viewCount;
+  }
 }
