869743c741a971c3ffa13cb933195fd5d24c8817
Gyeryeong Ban <gyeryeong.ban@outlook.com>
2021-08-31 00:22:46 +0900

    Update

--- project/study-with-us-project/app/src/main/java/com/studywithus/domain/Board.java
+++ project/study-with-us-project/app/src/main/java/com/studywithus/domain/Board.java
@@ -4,15 +4,25 @@ import java.sql.Date;
 
 public class Board {
 
-	public int no;
-	public String title;
-	public String name;
-	public String content;
-	public String writer;
-	public Date registeredDate;
-	public int like;
-	public int viewCount;
-	// 주석 
+  public int no;
+  public String title;
+  public String name;
+  public String content;
+  public String writer;
+  public Date registeredDate;
+  public int like;
+  public int viewCount;
+  // 주석 
+
+  public int no;
+  public String title;
+  public String name;
+  public String content;
+  public String writer;
+  public Date registeredDate;
+  public int like;
+  public int viewCount;
+
 
   public int no;
   public String title;
