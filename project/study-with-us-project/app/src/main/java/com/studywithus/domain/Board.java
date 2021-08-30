89f011a0c861a6dd36cc9d0316f0268bb82d05a4
Gyeryeong Ban <gyeryeong.ban@outlook.com>
2021-08-31 00:16:52 +0900

    Update

--- project/study-with-us-project/app/src/main/java/com/studywithus/domain/Board.java
+++ project/study-with-us-project/app/src/main/java/com/studywithus/domain/Board.java
@@ -13,5 +13,14 @@ public class Board {
   public int like;
   public int viewCount;
 
+  public int no;
+  public String title;
+  public String name;
+  public String content;
+  public String writer;
+  public Date registeredDate;
+  public int like;
+  public int viewCount;
+
 }
 
