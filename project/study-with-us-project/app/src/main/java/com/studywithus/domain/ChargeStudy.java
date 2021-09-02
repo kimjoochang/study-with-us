49f992e1e9fe20549cf74403a306e48fc5dea4c8
SeonyoungHa <syh5004@naver.com>
2021-09-02 14:36:11 +0900

    Update ChargeStudy

--- project/study-with-us-project/app/src/main/java/com/studywithus/domain/ChargeStudy.java
+++ project/study-with-us-project/app/src/main/java/com/studywithus/domain/ChargeStudy.java
@@ -4,113 +4,114 @@ import java.sql.Date;
 
 public class ChargeStudy {
 
-  private int no; // 번호
-  private String writer; // 멘토
-  private String area; // 지역
-  private String explanation; // 스터디 설명
-  private Date registeredDate; // 등록일
-  private int like; // 좋아요수
-  private int viewCount; // 조회수
-  private String mentorExplanation; // 멘토 설명
-  private String review; // 후기
-  private int price; // 가격
-  //	public String payment; // 결제
-  private String termsCondition; //약관동의
 
-  public int getNo() {
-    return no;
-  }
+	private String title; // 스터디 제목
+	private int no; // 번호
+	private String writer; // 멘토
+	private String area; // 지역
+	private String explanation; // 스터디 설명
+	private Date registeredDate; // 등록일
+	private int like; // 좋아요수
+	private int viewCount; // 조회수
+	private String mentorExplanation; // 멘토 설명
+	private String review; // 후기
+	private int price; // 가격
+	//	public String payment; // 결제
+	private String termsCondition; //약관동의
 
-  public void setNo(int no) {
-    this.no = no;
-  }
+	public int getNo() {
+		return no;
+	}
 
-  private String title; // 스터디 제목
-  public String getTitle() {
-    return title;
-  }
+	public void setNo(int no) {
+		this.no = no;
+	}
 
-  public void setTitle(String title) {
-    this.title = title;
-  }
+	public String getTitle() {
+		return title;
+	}
 
-  public String getWriter() {
-    return writer;
-  }
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-  public void setWriter(String writer) {
-    this.writer = writer;
-  }
+	public String getWriter() {
+		return writer;
+	}
 
-  public String getArea() {
-    return area;
-  }
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
 
-  public void setArea(String area) {
-    this.area = area;
-  }
+	public String getArea() {
+		return area;
+	}
 
-  public String getExplanation() {
-    return explanation;
-  }
+	public void setArea(String area) {
+		this.area = area;
+	}
 
-  public void setExplanation(String explanation) {
-    this.explanation = explanation;
-  }
+	public String getExplanation() {
+		return explanation;
+	}
 
-  public Date getRegisteredDate() {
-    return registeredDate;
-  }
+	public void setExplanation(String explanation) {
+		this.explanation = explanation;
+	}
 
-  public void setRegisteredDate(Date registeredDate) {
-    this.registeredDate = registeredDate;
-  }
+	public Date getRegisteredDate() {
+		return registeredDate;
+	}
 
-  public int getLike() {
-    return like;
-  }
+	public void setRegisteredDate(Date registeredDate) {
+		this.registeredDate = registeredDate;
+	}
 
-  public void setLike(int like) {
-    this.like = like;
-  }
+	public int getLike() {
+		return like;
+	}
 
-  public int getViewCount() {
-    return viewCount;
-  }
+	public void setLike(int like) {
+		this.like = like;
+	}
 
-  public void setViewCount(int viewCount) {
-    this.viewCount = viewCount;
-  }
+	public int getViewCount() {
+		return viewCount;
+	}
 
-  public String getMentorExplanation() {
-    return mentorExplanation;
-  }
+	public void setViewCount(int viewCount) {
+		this.viewCount = viewCount;
+	}
 
-  public void setMentorExplanation(String mentorExplanation) {
-    this.mentorExplanation = mentorExplanation;
-  }
+	public String getMentorExplanation() {
+		return mentorExplanation;
+	}
 
-  public String getReview() {
-    return review;
-  }
+	public void setMentorExplanation(String mentorExplanation) {
+		this.mentorExplanation = mentorExplanation;
+	}
 
-  public void setReview(String review) {
-    this.review = review;
-  }
+	public String getReview() {
+		return review;
+	}
 
-  public int getPrice() {
-    return price;
-  }
+	public void setReview(String review) {
+		this.review = review;
+	}
 
-  public void setPrice(int price) {
-    this.price = price;
-  }
+	public int getPrice() {
+		return price;
+	}
 
-  public String getTermsCondition() {
-    return termsCondition;
-  }
+	public void setPrice(int price) {
+		this.price = price;
+	}
 
-  public void setTermsCondition(String termsCondition) {
-    this.termsCondition = termsCondition;
-  }
+	public String getTermsCondition() {
+		return termsCondition;
+	}
+
+	public void setTermsCondition(String termsCondition) {
+		this.termsCondition = termsCondition;
+	}
 }
