6a814a0f390996507dcc4f2b5912a509922c10e9
kimjoochang <kimjc0707@naver.com>
2021-09-04 12:11:41 +0900

    Upload AuthLogoutHandler && Update App

---
+++ project/study-with-us-project/app/src/main/java/com/studywithus/handler/AuthLogoutHandler.java
@@ -0,0 +1,11 @@
+package com.studywithus.handler;
+
+public class AuthLogoutHandler implements Command {
+  @Override
+  public void execute() {
+    System.out.println("[로그아웃]");
+
+    AuthLoginHandler.loginUser = null;
+    System.out.println("로그아웃 하였습니다.");
+  }
+}
