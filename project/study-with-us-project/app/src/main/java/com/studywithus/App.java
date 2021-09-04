da709867c5baab01226fcc29c6f0db03e652971b
kimjoochang <kimjc0707@naver.com>
2021-09-04 16:51:22 +0900

    Apply InterestMenuList In App

--- project/study-with-us-project/app/src/main/java/com/studywithus/App.java
+++ project/study-with-us-project/app/src/main/java/com/studywithus/App.java
@@ -1,19 +1,32 @@
 package com.studywithus;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import com.studywithus.domain.ChargeStudy;
+import com.studywithus.domain.FreeStudy;
 import com.studywithus.domain.NewMember;
 import com.studywithus.handler.AuthLoginHandler;
 import com.studywithus.handler.AuthLogoutHandler;
+import com.studywithus.handler.InterestChargeAddHandler;
+import com.studywithus.handler.InterestChargeDeleteHandler;
+import com.studywithus.handler.InterestFreeAddHandler;
+import com.studywithus.handler.InterestFreeDeleteHandler;
 import com.studywithus.menu.Menu;
 import com.studywithus.menu.MenuGroup;
 import com.studywithus.util.Prompt;
 
 public class App {
   List<NewMember> memberList = new LinkedList<>();
+  List<FreeStudy> freeInterestList = new ArrayList<>();
+  List<ChargeStudy> chargeInterestList = new ArrayList<>();
 
   AuthLoginHandler authLoginHandler = new AuthLoginHandler(memberList);
   AuthLogoutHandler authLogoutHandler = new AuthLogoutHandler();
+  InterestFreeAddHandler interestFreeAddHandler = new InterestFreeAddHandler(freeInterestList);
+  InterestFreeDeleteHandler interestFreeDeleteHandler = new InterestFreeDeleteHandler(freeInterestList);
+  InterestChargeAddHandler interestChargeAddHandler = new InterestChargeAddHandler(chargeInterestList);
+  InterestChargeDeleteHandler interestChargeDeleteHandler = new InterestChargeDeleteHandler(chargeInterestList);
 
   public static void main(String[] args) {
     App app = new App(); 
@@ -35,13 +48,47 @@ public class App {
       }
     });
 
-
     mainMenuGroup.add(new Menu("로그아웃", Menu.ENABLE_LOGIN) {
       @Override
       public void execute() {
         authLogoutHandler.execute(); 
       }
     });
+
+    MenuGroup interestMenu = new MenuGroup("관심목록");
+    mainMenuGroup.add(interestMenu);
+
+    MenuGroup freeInterestMenu = new MenuGroup("무료스터디 관심목록");
+    interestMenu.add(freeInterestMenu);
+
+    freeInterestMenu.add(new Menu("조회") {
+      @Override
+      public void execute() {
+        interestFreeAddHandler.execute(); 
+      }});
+
+    freeInterestMenu.add(new Menu("삭제") {
+      @Override
+      public void execute() {
+        interestFreeDeleteHandler.execute(); 
+      }});
+
+    MenuGroup chargeInterestMenu = new MenuGroup("유료스터디 관심목록");
+    interestMenu.add(chargeInterestMenu);
+
+    chargeInterestMenu.add(new Menu("조회") {
+      @Override
+      public void execute() {
+        interestFreeAddHandler.execute(); 
+      }});
+
+    chargeInterestMenu.add(new Menu("삭제") {
+      @Override
+      public void execute() {
+        interestFreeDeleteHandler.execute(); 
+      }});
+
+
     return mainMenuGroup;
   }
 }
