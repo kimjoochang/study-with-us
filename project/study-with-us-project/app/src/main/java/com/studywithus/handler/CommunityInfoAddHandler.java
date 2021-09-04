165041902a7339cacf59d9bb5a80b57279255f6d
jei-kim <jeje2kim@gmail.com>
2021-09-04 16:52:03 +0900

    Update by Jei

--- project/study-with-us-project/app/src/main/java/com/studywithus/handler/CommunityInfoAddHandler.java
+++ project/study-with-us-project/app/src/main/java/com/studywithus/handler/CommunityInfoAddHandler.java
@@ -2,29 +2,29 @@ package com.studywithus.handler;
 
 import java.sql.Date;
 import java.util.List;
-import com.studywithus.domain.Community;
+import com.studywithus.domain.CommunityInfo;
 import com.studywithus.util.Prompt;
 
-public class CommunityInfoAddHandler extends AbstractCommunityHandler{
+public class CommunityInfoAddHandler extends AbstractCommunityInfoHandler{
 
-  public CommunityInfoAddHandler(List<Community> communityList) {
-    super(communityList);
+  public CommunityInfoAddHandler(List<CommunityInfo> communityInfoList) {
+    super(communityInfoList);
   }
 
   // 정보 게시글 생성
   @Override
   public void execute() {
-    System.out.println("[커뮤니티 / 정보 / 생성] \n");
+    // System.out.println("[커뮤니티 / 정보 / 생성] \n");
 
-    Community community = new Community();
+    CommunityInfo communityInfo = new CommunityInfo();
 
-    community.setNo(Prompt.inputInt("번호? "));
-    community.setTitle(Prompt.inputString("제목? "));
-    community.setContent(Prompt.inputString("내용? "));
-    community.setWriter(Prompt.inputString("작성자? "));
-    community.setRegisteredDate(new Date(System.currentTimeMillis()));
+    communityInfo.setNo(Prompt.inputInt("번호? "));
+    communityInfo.setTitle(Prompt.inputString("제목? "));
+    communityInfo.setContent(Prompt.inputString("내용? "));
+    communityInfo.setWriter(Prompt.inputString("작성자? "));
+    communityInfo.setRegisteredDate(new Date(System.currentTimeMillis()));
 
-    communityList.add(community);
+    communityInfoList.add(communityInfo);
 
     System.out.println();
     System.out.println("정보 게시글 등록이 완료되었습니다.\n");
