4690ac14682c7ec7060344eed744d3c9f7e63206
Gyeryeong Ban <gyeryeong.ban@outlook.com>
2021-09-08 00:37:38 +0900

    Update Menu

--- project/study-with-us-project/app/src/main/java/com/studywithus/menu/Menu.java
+++ project/study-with-us-project/app/src/main/java/com/studywithus/menu/Menu.java
@@ -4,15 +4,16 @@ public abstract class Menu {
 
   public static final int ACCESS_LOGOUT = 0x01;
   public static final int ACCESS_GENERAL = 0x02;
-  public static final int ACCESS_ADMIN = 0x04;
+  public static final int ACCESS_LEADER = 0x04;
   public static final int ACCESS_MENTOR = 0x08;
+  public static final int ACCESS_ADMIN = 0x10;
 
   String title;
 
   int accessScope;
 
   public Menu(String title) {
-    this(title, ACCESS_LOGOUT | ACCESS_GENERAL | ACCESS_ADMIN | ACCESS_MENTOR);
+    this(title, ACCESS_LOGOUT | ACCESS_GENERAL | ACCESS_LEADER | ACCESS_MENTOR | ACCESS_ADMIN);
   }
 
   public Menu(String title, int accessScope) {
