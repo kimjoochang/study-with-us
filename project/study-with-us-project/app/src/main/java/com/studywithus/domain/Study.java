b97222e78ec4653ab698162826d200cb6058332b
Gyeryeong Ban <gyeryeong.ban@outlook.com>
2021-09-09 16:47:43 +0900

    Update Study

--- project/study-with-us-project/app/src/main/java/com/studywithus/domain/Study.java
+++ project/study-with-us-project/app/src/main/java/com/studywithus/domain/Study.java
@@ -2,43 +2,55 @@ package com.studywithus.domain;
 
 import java.io.Serializable;
 import java.sql.Date;
-<<<<<<< HEAD
-=======
-import java.util.ArrayList;
->>>>>>> ee9e47b844db5a4950ba5ee6227576576a2f2906
 import java.util.List;
 
 @SuppressWarnings("serial")
 public class Study implements Serializable {
-<<<<<<< HEAD
-  // 팀원 or 멘토
+  // 팀장 or 멘토
   private Member writer;
+
+  // 팀원 or 멘티
+  private List<Member> members;
+
   // 게시글 번호
   private int no;
+
   // 제목
   private String title;
+
   // 스터디 설명
   private String explanation;
+
   // 멘토 설명
   private String mentorExplanation;
+
   // 스터디 규칙
   private String rule;
+
   // 유료 스터디 가격
   private int price;
+
   // 온라인 or 오프라인
   private int onOffLine;
+
   // 지역
   private String area;
+
   // 유료 스터디 후기
   private String review;
+
   // 스터디 등록일
   private Date registeredDate;
+
   // 좋아요
   private int like;
+
   // 조회수
   private int viewCount;
+
   // 무료 스터디 관심목록
   private List<Study> freeInterest;
+
   // 유료 스터디 관심목록
   private List<Study> chargeInterest;
 
@@ -48,6 +60,14 @@ public class Study implements Serializable {
 
   public void setWriter(Member writer) {
     this.writer = writer;
+  }
+
+  public List<Member> getMembers() {
+    return members;
+  }
+
+  public void setMembers(List<Member> members) {
+    this.members = members;
   }
 
   public int getNo() {
@@ -161,140 +181,4 @@ public class Study implements Serializable {
   public void setChargeInterest(List<Study> chargeInterest) {
     this.chargeInterest = chargeInterest;
   }
-=======
-
-	private int no; // 번호
-	private String title; // 스터디 제목
-	private String writer; // 멘토
-	private String area; // 지역
-	private String explanation; // 스터디 설명
-	private Date registeredDate; // 등록일
-	private int like; // 좋아요수
-	private int viewCount; // 조회수
-
-	private String mentorExplanation; // 멘토 설명
-	private String review; // 후기
-	private int price; // 가격
-	//	public String payment; // 결제
-	private String termsCondition; //약관동의
-
-	private int onOffLine; // 온오프라인
-	private String rule; // 스터디 
-
-	private List<Member> members = new ArrayList<Member>();
-
-	private List<Study> pay;
-
-	public int getNo() {
-		return no;
-	}
-
-	public void setNo(int no) {
-		this.no = no;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getWriter() {
-		return writer;
-	}
-
-	public void setWriter(String writer) {
-		this.writer = writer;
-	}
-
-	public String getArea() {
-		return area;
-	}
-
-	public void setArea(String area) {
-		this.area = area;
-	}
-
-	public String getExplanation() {
-		return explanation;
-	}
-
-	public void setExplanation(String explanation) {
-		this.explanation = explanation;
-	}
-
-	public Date getRegisteredDate() {
-		return registeredDate;
-	}
-
-	public void setRegisteredDate(Date registeredDate) {
-		this.registeredDate = registeredDate;
-	}
-
-	public int getLike() {
-		return like;
-	}
-
-	public void setLike(int like) {
-		this.like = like;
-	}
-
-	public int getViewCount() {
-		return viewCount;
-	}
-
-	public void setViewCount(int viewCount) {
-		this.viewCount = viewCount;
-	}
-
-	public String getMentorExplanation() {
-		return mentorExplanation;
-	}
-
-	public void setMentorExplanation(String mentorExplanation) {
-		this.mentorExplanation = mentorExplanation;
-	}
-
-	public String getReview() {
-		return review;
-	}
-
-	public void setReview(String review) {
-		this.review = review;
-	}
-
-	public int getPrice() {
-		return price;
-	}
-
-	public void setPrice(int price) {
-		this.price = price;
-	}
-
-	public String getTermsCondition() {
-		return termsCondition;
-	}
-
-	public void setTermsCondition(String termsCondition) {
-		this.termsCondition = termsCondition;
-	}
-
-	public int getOnOffLine() {
-		return onOffLine;
-	}
-
-	public void setOnOffLine(int onOffLine) {
-		this.onOffLine = onOffLine;
-	}
-
-	public String getRule() {
-		return rule;
-	}
-
-	public void setRule(String rule) {
-		this.rule = rule;
-	}
->>>>>>> ee9e47b844db5a4950ba5ee6227576576a2f2906
 }
