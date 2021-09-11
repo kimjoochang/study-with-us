fbf431332be15c4421697928f8a566434075412b
Gyeryeong Ban <gyeryeong.ban@outlook.com>
2021-09-11 18:12:38 +0900

    Update App

--- project/study-with-us-project/app/src/main/java/com/studywithus/handler/AuthLogoutHandler.java
+++ project/study-with-us-project/app/src/main/java/com/studywithus/handler/AuthLogoutHandler.java
@@ -7,14 +7,14 @@ import com.studywithus.menu.Menu;
 public class AuthLogoutHandler implements Command {
 
   public AuthLogoutHandler(List <Member> memberlist) {
-
   }
+
   @Override
   public void execute() {
     System.out.println("[로그아웃]");
 
-    // 로그아웃 시 변경된 권한 업데이트
-    member.setUserAccessLevel(AuthLoginHandler.userAccessLevel);
+    // 현재 로그인된 회원의 권한을 setUserAccessLevel에 저장 후 로그아웃
+    AuthLoginHandler.loginUser.setUserAccessLevel(AuthLoginHandler.userAccessLevel);
 
     AuthLoginHandler.loginUser = null;
     AuthLoginHandler.userAccessLevel = Menu.ACCESS_LOGOUT;
