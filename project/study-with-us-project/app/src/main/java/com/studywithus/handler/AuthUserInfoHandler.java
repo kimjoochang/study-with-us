c6ecccf3357871beec58a98740dc8abd7aa98b47
SeonyoungHa <syh5004@naver.com>
2021-09-14 23:38:11 +0900

    Update AuthUserInfoHandler

--- project/study-with-us-project/app/src/main/java/com/studywithus/handler/AuthUserInfoHandler.java
+++ project/study-with-us-project/app/src/main/java/com/studywithus/handler/AuthUserInfoHandler.java
@@ -20,6 +20,32 @@ public class AuthUserInfoHandler implements Command {
 			return;
 		}
 
+		switch(loginUser.getUserAccessLevel()) {
+		case 0x02: // 회원
+			System.out.println("등급: 회원");
+			break;
+
+		case 0x04: // 팀원
+			System.out.println("등급: 회원");
+			break;
+
+		case 0x08: // 팀장
+			System.out.println("등급: 회원");
+			break;
+
+		case 0x40: // 멘티
+			System.out.println("등급: 회원");
+			break;
+
+		case 0x10: // 멘토
+			System.out.println("등급: 멘토");
+			break;
+
+		case 0x20: // 관리자
+			System.out.println("등급: 관리자");
+			break;
+		}
+
 		System.out.printf("이름: %s\n", loginUser.getName());
 		System.out.printf("이메일: %s\n", loginUser.getEmail());
 		System.out.printf("휴대폰 번호: %s\n", loginUser.getPhoneNumber());
