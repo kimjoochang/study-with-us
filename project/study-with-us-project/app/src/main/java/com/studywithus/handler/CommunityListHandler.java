823aa4d4bffd8ef7945e5ea7cc0a8a139d797783
jei-kim <jeje2kim@gmail.com>
2021-09-14 19:42:12 +0900

    Update CommunityListHandler

--- project/study-with-us-project/app/src/main/java/com/studywithus/handler/CommunityListHandler.java
+++ project/study-with-us-project/app/src/main/java/com/studywithus/handler/CommunityListHandler.java
@@ -13,7 +13,21 @@ public class CommunityListHandler extends AbstractCommunityHandler{
   public void execute() {
     System.out.println("[커뮤니티 / 조회]\n");
 
+    //    Community community;
+    //    if (community == null) {
+    //      System.out.println();
+    //      System.out.println("등록된 게시글이 없습니다.\n");
+    //      return;
+    //    }
+
     for (Community community : communityList) {
+
+      if (community == null) {
+        System.out.println();
+        System.out.println("등록된 게시글이 없습니다.\n");
+        return;
+      }
+
       System.out.printf("[번호 = %d, 제목 = %s, 작성자 = %s, 등록일 = %s, 조회수 = %d, 좋아요 = %d]\n", 
           community.getNo(),
           community.getTitle(), 
