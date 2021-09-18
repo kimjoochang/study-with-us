4d034076237b3e323c513e35a4f806d5cb0fc7bd
Gyeryeong Ban <gyeryeong.ban@outlook.com>
2021-09-18 15:41:10 +0900

    Update ParticipateFreeStudyListHandler

--- project/study-with-us-project/app/src/main/java/com/studywithus/handler/ParticipateFreeStudyListHandler.java
+++ project/study-with-us-project/app/src/main/java/com/studywithus/handler/ParticipateFreeStudyListHandler.java
@@ -29,39 +29,5 @@ public class ParticipateFreeStudyListHandler implements Command {
           freeStudy.getViewCount(), 
           freeStudy.getLike());
     }
-    // 상세보기 기능 안하기로 함
-
-    //    // 내가 참여한 무료 스터디 상세보기
-    //    System.out.println();
-    //    String title = Prompt.inputString("제목: ");
-    //
-    //    Study freeStudy = findByName(title, myParticipatedFreeStudy);
-    //
-    //    if (freeStudy == null) {
-    //      System.out.println();
-    //      System.out.println("입력하신 제목과 일치하는 스터디가 없습니다.\n");
-    //      return;
-    //    }
-    //
-    //    System.out.printf("제목: %s\n", freeStudy.getTitle());
-    //
-    //    if (freeStudy.getArea() != null) {
-    //      System.out.printf("지역: %s\n", freeStudy.getArea());
-    //    }
-    //
-    //    System.out.printf("설명: %s\n", freeStudy.getContent());
-    //    System.out.printf("규칙: %s\n", freeStudy.getRule());
-    //    System.out.printf("등록일: %s\n", freeStudy.getRegisteredDate());
-    //    System.out.printf("조회수: %d\n", freeStudy.getViewCount());
-
   }
-  //상세보기 기능 안하기로 함
-  //  private Study findByName(String title, List<Study> myParticipatedFreeStudyMap) {
-  //    for (Study freeStudy : myParticipatedFreeStudyMap) {
-  //      if (freeStudy.getTitle().equals(title)) {
-  //        return freeStudy;
-  //      }
-  //    }
-  //    return null;
-  //  }
 }
