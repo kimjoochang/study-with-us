e57771ea3c31232ce6fc5a2e78b50a1d827c851e
Gyeryeong Ban <gyeryeong.ban@outlook.com>
2021-09-22 14:14:55 +0900

    Update ExamCalendarAddHandler/JobsCalendarAddHandler

--- project/study-with-us-project/app/src/main/java/com/studywithus/handler/calendar/ExamCalendarAddHandler.java
+++ project/study-with-us-project/app/src/main/java/com/studywithus/handler/calendar/ExamCalendarAddHandler.java
@@ -1,5 +1,6 @@
 package com.studywithus.handler.calendar;
 
+import java.util.GregorianCalendar;
 import java.util.List;
 import com.studywithus.domain.Calendar;
 import com.studywithus.handler.CommandRequest;
@@ -7,6 +8,8 @@ import com.studywithus.handler.user.AuthLogInHandler;
 import com.studywithus.util.Prompt;
 
 public class ExamCalendarAddHandler extends AbstractCalendarHandler {
+
+  GregorianCalendar calendar = new GregorianCalendar();
 
   public ExamCalendarAddHandler(List<Calendar> examCalendarList) {
     super(examCalendarList);
@@ -24,9 +27,86 @@ public class ExamCalendarAddHandler extends AbstractCalendarHandler {
     examCalendar.setContent(Prompt.inputString("내용을 입력하세요. > "));
 
     System.out.println("시험일을 입력하세요.");
-    examCalendar.setYyyy(Prompt.inputInt("YYYY > "));
-    examCalendar.setMm(Prompt.inputInt("MM > "));
-    examCalendar.setDd(Prompt.inputInt("DD > "));
+
+    // 년
+    while (true) {
+      examCalendar.setYyyy(Prompt.inputInt("YYYY > "));
+
+      if (examCalendar.getYyyy() < 2021) {
+        System.out.println("유효한 연도를 입력하시오.");
+        continue;
+
+      } else {
+        break;
+      }
+    }
+
+    // 월
+    while (true) {
+      examCalendar.setMm(Prompt.inputInt("MM > "));
+
+      if (!(1 <= examCalendar.getMm() && examCalendar.getMm() <= 12)) {
+        System.out.println("유효한 월을 입력하시오.");
+        continue;
+
+      } else {
+        break;
+      }
+    }
+
+    // 일
+    while (true) {
+      examCalendar.setDd(Prompt.inputInt("DD > "));
+
+      // 1 ~ 31일
+      switch (examCalendar.getMm()) {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+
+          if (!(examCalendar.getDd() >= 1 && examCalendar.getDd() <= 31)) {
+            System.out.println("유효한 일을 입력하시오.");
+            continue;
+
+          } else {
+            break;
+          }
+      }
+
+      // 1 ~ 30일
+      switch (examCalendar.getMm()) {
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+
+          if (!(examCalendar.getDd() >= 1 && examCalendar.getDd() <= 30)) {
+            System.out.println("유효한 일을 입력하시오.");
+            continue;
+
+          } else {
+            break;
+          }
+      }
+
+      // 1 ~ 28일
+      switch (examCalendar.getMm()) {
+        case 2:
+
+          if (!(examCalendar.getDd() >= 1 && examCalendar.getDd() <= 28)) {
+            System.out.println("유효한 일을 입력하시오.");
+            continue;
+
+          } else {
+            break;
+          }
+      }
+      break;
+    }
 
     calendarList.add(examCalendar);
 
--- project/study-with-us-project/app/src/main/java/com/studywithus/handler/calendar/JobsCalendarAddHandler.java
+++ project/study-with-us-project/app/src/main/java/com/studywithus/handler/calendar/JobsCalendarAddHandler.java
@@ -24,14 +24,168 @@ public class JobsCalendarAddHandler extends AbstractCalendarHandler {
     jobsCalendar.setContent(Prompt.inputString("내용을 입력하세요. > "));
 
     System.out.println("시작일을 입력하세요.");
-    jobsCalendar.setStartYyyy(Prompt.inputInt("YYYY > "));
-    jobsCalendar.setStartMm(Prompt.inputInt("MM > "));
-    jobsCalendar.setStartDd(Prompt.inputInt("DD > "));
+
+    // 년
+    while (true) {
+      jobsCalendar.setStartYyyy(Prompt.inputInt("YYYY > "));
+
+      if (jobsCalendar.getStartYyyy() < 2021) {
+        System.out.println("유효한 연도를 입력하시오.");
+        continue;
+
+      } else {
+        break;
+      }
+    }
+
+    // 월
+    while (true) {
+      jobsCalendar.setStartMm(Prompt.inputInt("MM > "));
+
+      if (!(1 <= jobsCalendar.getStartMm() && jobsCalendar.getStartMm() <= 12)) {
+        System.out.println("유효한 월을 입력하시오.");
+        continue;
+
+      } else {
+        break;
+      }
+    }
+
+    // 일
+    while (true) {
+      jobsCalendar.setStartDd(Prompt.inputInt("DD > "));
+
+      // 1 ~ 31일
+      switch (jobsCalendar.getStartMm()) {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+
+          if (!(jobsCalendar.getStartDd() >= 1 && jobsCalendar.getStartDd() <= 31)) {
+            System.out.println("유효한 일을 입력하시오.");
+            continue;
+
+          } else {
+            break;
+          }
+      }
+
+      // 1 ~ 30일
+      switch (jobsCalendar.getStartMm()) {
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+
+          if (!(jobsCalendar.getStartDd() >= 1 && jobsCalendar.getStartDd() <= 30)) {
+            System.out.println("유효한 일을 입력하시오.");
+            continue;
+
+          } else {
+            break;
+          }
+      }
+
+      // 1 ~ 28일
+      switch (jobsCalendar.getStartMm()) {
+        case 2:
+
+          if (!(jobsCalendar.getStartDd() >= 1 && jobsCalendar.getStartDd() <= 28)) {
+            System.out.println("유효한 일을 입력하시오.");
+            continue;
+
+          } else {
+            break;
+          }
+      }
+      break;
+    }
 
     System.out.println("종료일을 입력하세요.");
-    jobsCalendar.setYyyy(Prompt.inputInt("YYYY > "));
-    jobsCalendar.setMm(Prompt.inputInt("MM > "));
-    jobsCalendar.setDd(Prompt.inputInt("DD > "));
+
+    // 년
+    while (true) {
+      jobsCalendar.setYyyy(Prompt.inputInt("YYYY > "));
+
+      if (jobsCalendar.getYyyy() < 2021) {
+        System.out.println("유효한 연도를 입력하시오.");
+        continue;
+
+      } else {
+        break;
+      }
+    }
+
+    // 월
+    while (true) {
+      jobsCalendar.setMm(Prompt.inputInt("MM > "));
+
+      if (!(1 <= jobsCalendar.getMm() && jobsCalendar.getMm() <= 12)) {
+        System.out.println("유효한 월을 입력하시오.");
+        continue;
+
+      } else {
+        break;
+      }
+    }
+
+    // 일
+    while (true) {
+      jobsCalendar.setDd(Prompt.inputInt("DD > "));
+
+      // 1 ~ 31일
+      switch (jobsCalendar.getMm()) {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+
+          if (!(jobsCalendar.getDd() >= 1 && jobsCalendar.getDd() <= 31)) {
+            System.out.println("유효한 일을 입력하시오.");
+            continue;
+
+          } else {
+            break;
+          }
+      }
+
+      // 1 ~ 30일
+      switch (jobsCalendar.getMm()) {
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+
+          if (!(jobsCalendar.getDd() >= 1 && jobsCalendar.getDd() <= 30)) {
+            System.out.println("유효한 일을 입력하시오.");
+            continue;
+
+          } else {
+            break;
+          }
+      }
+
+      // 1 ~ 28일
+      switch (jobsCalendar.getMm()) {
+        case 2:
+
+          if (!(jobsCalendar.getDd() >= 1 && jobsCalendar.getDd() <= 28)) {
+            System.out.println("유효한 일을 입력하시오.");
+            continue;
+
+          } else {
+            break;
+          }
+      }
+      break;
+    }
 
     calendarList.add(jobsCalendar);
 
