1ae09e0deacfaf4b54e64cbfc155597f84bd4f93
SeonyoungHa <syh5004@naver.com>
2021-09-23 16:20:39 +0900

    Update ParticipateChargeStudyListHandler

--- project/study-with-us-project/app/src/main/java/com/studywithus/handler/study/ParticipateChargeStudyListHandler.java
+++ project/study-with-us-project/app/src/main/java/com/studywithus/handler/study/ParticipateChargeStudyListHandler.java
@@ -18,8 +18,12 @@ public class ParticipateChargeStudyListHandler implements Command {
 	}
 
 	@Override
-	public void execute(CommandRequest request) {
-		System.out.println("[마이 페이지 / 나의 활동 / 내 스터디 / 참여 유료 스터디]\n");
+	public void execute(CommandRequest request) { 
+		/* 해쉬맵의 value값을 myParticipatedFreeStudy에 담음 
+		 * 전역변수로 둘 경우 App 실행 시 getLoginUser() nullPointer 에러뜸 */
+		List<Study> myParticipatedChargeStudy = participateChargeStudyMap.get(AuthLogInHandler.getLoginUser().getId());
+
+		System.out.println("[마이 페이지 / 내가 참여한 유료 스터디]\n");
 
 		if (participateChargeStudyMap.isEmpty() == true) {
 			System.out.println("유료 스터디 게시글이 존재하지 않습니다.\n");
@@ -27,14 +31,16 @@ public class ParticipateChargeStudyListHandler implements Command {
 		}
 
 		// 회원 아이디로 값 호출
-		for (Study chargeStudy : participateChargeStudyMap.get(AuthLogInHandler.getLoginUser().getId())) {
-			System.out.printf("[번호 = %d, 제목 = %s, 멘토 = %s, 등록일 = %s, 조회수 = %d, 좋아요 = %d]",
+		for (Study chargeStudy : myParticipatedChargeStudy) {
+			System.out.printf("[번호 = %d, 제목 = %s, 멘토 = %s, 등록일 = %s, 모집인원 = %d / %d, 조회수 = %d, 좋아요 = %d]\n",
 					chargeStudy.getNo(),
 					chargeStudy.getTitle(),
 					chargeStudy.getWriter().getName(),
 					chargeStudy.getRegisteredDate(),
+					chargeStudy.getMembers().size(),
+					chargeStudy.getMaxMembers(),
 					chargeStudy.getViewCount(),
-					chargeStudy.getLikeMembers());
+					chargeStudy.getLikeMembers().size());
 		}
 		System.out.println();
 	}
