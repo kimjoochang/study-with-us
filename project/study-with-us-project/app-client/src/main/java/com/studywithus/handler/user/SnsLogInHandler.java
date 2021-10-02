f1218187d9395c293ef41d947974d661cab1a3a4
jei-kim <jeje2kim@gmail.com>
2021-10-02 14:13:10 +0900

    Create ClientAppJJ and ServerAppJJ

--- project/study-with-us-project/app-client/src/main/java/com/studywithus/handler/user/SnsLogInHandler.java
+++ project/study-with-us-project/app-client/src/main/java/com/studywithus/handler/user/SnsLogInHandler.java
@@ -1,15 +1,16 @@
 package com.studywithus.handler.user;
 
-import java.util.List;
+import java.util.HashMap;
 import com.studywithus.domain.Member;
 import com.studywithus.handler.Command;
 import com.studywithus.handler.CommandRequest;
 import com.studywithus.menu.Menu;
+import com.studywithus.request.RequestAgent;
 import com.studywithus.util.Prompt;
 
 public class SnsLogInHandler implements Command {
 
-  List<Member> memberList;
+  RequestAgent requestAgent;
 
   static Member loginUser;
   static int userAccessLevel = Menu.ACCESS_LOGOUT;
@@ -22,38 +23,32 @@ public class SnsLogInHandler implements Command {
     return userAccessLevel;
   }
 
-  public SnsLogInHandler(List<Member> memberList) {
-    this.memberList = memberList;
+  public SnsLogInHandler(RequestAgent requestAgent) {
+    this.requestAgent = requestAgent;
   }
 
   @Override
-  public void execute(CommandRequest request) {
+  public void execute(CommandRequest request) throws Exception {
     System.out.println("[SNS 로그인]\n");
 
-    String snsId = Prompt.inputString("아이디: ");
+    String snsEmail = Prompt.inputString("아이디: ");
     String password = Prompt.inputString("비밀번호: ");
 
-    Member member = findBySnsIdPassword(snsId, password);
+    HashMap<String,String> params = new HashMap<>();
+    params.put("email", snsEmail);
+    params.put("password", password);
 
-    if (member == null) {
-      System.out.println("");
-      System.out.println("연동된 SNS 계정이 존재하지 않습니다.\n");
+    requestAgent.request("member.selectOneByEmailPassword", params);
+
+    if (requestAgent.getStatus().equals(RequestAgent.SUCCESS)) {
+      Member member = requestAgent.getObject(Member.class);
+      System.out.printf("%s님 환영합니다!\n", member.getName());
+      loginUser = member;
+      userAccessLevel = Menu.ACCESS_GENERAL;
 
     } else {
-      System.out.println("");
-      System.out.printf("%s님 환영합니다.\n", member.getName());
-
-      AuthLogInHandler.loginUser = member;
-      AuthLogInHandler.userAccessLevel = member.getUserAccessLevel();
+      System.out.println("이메일과 암호가 일치하는 회원을 찾을 수 없습니다.");
     }
-  }
 
-  private Member findBySnsIdPassword(String SnsId, String SnsPassword) {
-    for (Member member : memberList) {
-      if (member.getId().equalsIgnoreCase(SnsId) && member.getPassword().equals(SnsPassword)) {
-        return member;
-      }
-    }
-    return null;
   }
 }
