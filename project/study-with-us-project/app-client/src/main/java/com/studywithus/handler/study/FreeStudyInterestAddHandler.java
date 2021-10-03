8d0f56c625b1c2339a75d5df8fae22ca392d347a
Gyeryeong Ban <gyeryeong.ban@outlook.com>
2021-10-03 17:00:23 +0900

    Create FreeStudyInterestAddHandler

--- project/study-with-us-project/app-client/src/main/java/com/studywithus/handler/study/FreeStudyInterestAddHandler.java
+++ project/study-with-us-project/app-client/src/main/java/com/studywithus/handler/study/FreeStudyInterestAddHandler.java
@@ -1,42 +1,36 @@
 package com.studywithus.handler.study;
 
-import java.util.Collection;
 import java.util.List;
 import com.studywithus.domain.Member;
 import com.studywithus.domain.Study;
-import com.studywithus.handler.Command;
 import com.studywithus.handler.CommandRequest;
 import com.studywithus.handler.user.AuthLogInHandler;
-import com.studywithus.request.RequestAgent;
 import com.studywithus.util.Prompt;
 
-public class FreeStudyInterestAddHandler implements Command {
+public class FreeStudyInterestAddHandler extends AbstractStudyHandler {
 
-  RequestAgent requestAgent;
 
-  public FreeStudyInterestAddHandler(RequestAgent requestAgent) {
-    this.requestAgent = requestAgent;
+  public FreeStudyInterestAddHandler(List<Study> freeStudyList) {
+    super(freeStudyList);
   }
 
   @Override
-  public void execute(CommandRequest request) throws Exception {
-    System.out.println("[마이페이지 / 무료 스터디 / 관심 목록 / 추가]\n");
+  public void execute(CommandRequest request) {
+    System.out.println("[무료 스터디 / 상세보기 / 관심 목록]\n");
     int no = (int) request.getAttribute("freeNo");
 
-    // Study freeInterest = findByNo(no);
+    Study freeInterest = findByNo(no);
 
-    requestAgent.request("freeStudy.selectList", null);
 
-    // if (freeInterest == null) {
-    // System.out.println("해당 번호의 게시글이 없습니다.");
-    // return;
-    // }
-
+    if (freeInterest == null) {
+      System.out.println("해당 번호의 게시글이 없습니다.");
+      return;
+    }
     while (true) {
       String input = Prompt.inputString("무료 스터디 관심 목록에 추가하시겠습니까? (y/N) ");
 
       if (input.equalsIgnoreCase("n") || input.length() == 0) {
-        System.out.println("무료 스터디 관심 목록 추가를 취소하였습니다.");
+        System.out.println("무료 스터디 관심 목록 추가를 취소하였습니다.\n");
         return;
 
       } else if (!input.equalsIgnoreCase("y")) {
@@ -48,17 +42,9 @@ public class FreeStudyInterestAddHandler implements Command {
       }
     }
 
-    List<Member> likeUserList = freeInterest.getLikeMembers();
-    likeUserList.add(AuthLogInHandler.getLoginUser());
-    freeInterest.setLikeMembers(likeUserList);
-
-    Collection<Study> studyList = requestAgent.getObjects(Study.class);
-
-    requestAgent.request("freeStudy.insert", freeStudy);
-    if (requestAgent.getStatus().equals(RequestAgent.FAIL)) {
-      System.out.println("게시글 저장 실패!");
-      return;
-    }
+    List<Member> likeMember = freeInterest.getLikeMembers();
+    likeMember.add(AuthLogInHandler.getLoginUser());
+    freeInterest.setLikeMembers(likeMember);
 
     System.out.println();
     System.out.println("무료 스터디 관심 목록에 추가되었습니다.");
