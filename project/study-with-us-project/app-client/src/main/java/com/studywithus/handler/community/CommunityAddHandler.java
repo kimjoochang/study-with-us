6b4cd8acb39a72dd83cd8d8ddb5948a5de6ff05d
Gyeryeong Ban <gyeryeong.ban@outlook.com>
2021-10-03 09:48:30 +0900

    Update CommunityAddHandler, Create FreeStudyTable

--- project/study-with-us-project/app-client/src/main/java/com/studywithus/handler/community/CommunityAddHandler.java
+++ project/study-with-us-project/app-client/src/main/java/com/studywithus/handler/community/CommunityAddHandler.java
@@ -1,21 +1,23 @@
 package com.studywithus.handler.community;
 
 import java.sql.Date;
-import java.util.List;
 import com.studywithus.domain.Community;
+import com.studywithus.handler.Command;
 import com.studywithus.handler.CommandRequest;
 import com.studywithus.handler.user.AuthLogInHandler;
+import com.studywithus.request.RequestAgent;
 import com.studywithus.util.Prompt;
 
-public class CommunityAddHandler extends AbstractCommunityHandler{
+public class CommunityAddHandler implements Command {
 
-  public CommunityAddHandler(List<Community> communityList) {
-    super(communityList);
+  RequestAgent requestAgent;
 
+  public CommunityAddHandler(RequestAgent requestAgent) {
+    this.requestAgent = requestAgent;
   }
 
   @Override
-  public void execute(CommandRequest request) {
+  public void execute(CommandRequest request) throws Exception {
     System.out.println("[커뮤니티 / 생성] \n");
 
     Community community = new Community();
@@ -26,9 +28,15 @@ public class CommunityAddHandler extends AbstractCommunityHandler{
     community.setWriter(AuthLogInHandler.getLoginUser());
     community.setRegisteredDate(new Date(System.currentTimeMillis()));
 
-    communityList.add(community);
+    //    communityList.add(community);
+
+    requestAgent.request("community.insert", community);
+    if (requestAgent.getStatus().equals(RequestAgent.FAIL)) {
+      System.out.println("커뮤니티 저장 실패!");
+      return;
+    }
 
     System.out.println();
-    System.out.println("게시글 등록이 완료되었습니다.\n");
+    System.out.println("커뮤니티 등록이 완료되었습니다.\n");
   }
 }
