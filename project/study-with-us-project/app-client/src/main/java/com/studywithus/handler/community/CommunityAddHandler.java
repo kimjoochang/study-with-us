e7788a308309da4785dd0c66b578583f75c80a13
SeonyoungHa <syh5004@naver.com>
2021-10-03 12:15:55 +0900

    Updaet CommunityHandler

--- project/study-with-us-project/app-client/src/main/java/com/studywithus/handler/community/CommunityAddHandler.java
+++ project/study-with-us-project/app-client/src/main/java/com/studywithus/handler/community/CommunityAddHandler.java
@@ -1,6 +1,7 @@
 package com.studywithus.handler.community;
 
 import java.sql.Date;
+
 import com.studywithus.domain.Community;
 import com.studywithus.handler.Command;
 import com.studywithus.handler.CommandRequest;
@@ -10,33 +11,34 @@ import com.studywithus.util.Prompt;
 
 public class CommunityAddHandler implements Command {
 
-  RequestAgent requestAgent;
+	RequestAgent requestAgent;
+	int no = 1;
 
-  public CommunityAddHandler(RequestAgent requestAgent) {
-    this.requestAgent = requestAgent;
-  }
+	public CommunityAddHandler(RequestAgent requestAgent) {
+		this.requestAgent = requestAgent;
+	}
 
-  @Override
-  public void execute(CommandRequest request) throws Exception {
-    System.out.println("[커뮤니티 / 생성] \n");
+	@Override
+	public void execute(CommandRequest request) throws Exception {
+		System.out.println("[커뮤니티 / 생성] \n");
 
-    Community community = new Community();
+		Community community = new Community();
 
-    community.setNo(Prompt.inputInt("번호를 입력하세요. > "));
-    community.setTitle(Prompt.inputString("제목을 입력하세요. > "));
-    community.setContent(Prompt.inputString("내용을 입력하세요. > "));
-    community.setWriter(AuthLogInHandler.getLoginUser());
-    community.setRegisteredDate(new Date(System.currentTimeMillis()));
+		community.setNo(no++);
+		community.setTitle(Prompt.inputString("제목을 입력하세요. > "));
+		community.setContent(Prompt.inputString("내용을 입력하세요. > "));
+		community.setWriter(AuthLogInHandler.getLoginUser());
+		community.setRegisteredDate(new Date(System.currentTimeMillis()));
 
-    //    communityList.add(community);
+		//    communityList.add(community);
 
-    requestAgent.request("community.insert", community);
-    if (requestAgent.getStatus().equals(RequestAgent.FAIL)) {
-      System.out.println("커뮤니티 저장 실패!");
-      return;
-    }
+		requestAgent.request("community.insert", community);
+		if (requestAgent.getStatus().equals(RequestAgent.FAIL)) {
+			System.out.println("커뮤니티 저장 실패!");
+			return;
+		}
 
-    System.out.println();
-    System.out.println("커뮤니티 등록이 완료되었습니다.\n");
-  }
+		System.out.println();
+		System.out.println("커뮤니티 등록이 완료되었습니다.\n");
+	}
 }
