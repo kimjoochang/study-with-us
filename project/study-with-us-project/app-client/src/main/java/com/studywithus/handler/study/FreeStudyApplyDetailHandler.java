b624283dc8b2aef011c11cd0d83a8948d29ad94f
Gyeryeong Ban <gyeryeong.ban@outlook.com>
2021-10-07 21:00:23 +0900

    Debug FreeStudyApplyDetailHandler

--- project/study-with-us-project/app-client/src/main/java/com/studywithus/handler/study/FreeStudyApplyDetailHandler.java
+++ project/study-with-us-project/app-client/src/main/java/com/studywithus/handler/study/FreeStudyApplyDetailHandler.java
@@ -47,7 +47,6 @@ public class FreeStudyApplyDetailHandler implements Command {
     if (freeStudy.getArea() != null) {
       System.out.printf("온/오프라인: %s\n", freeStudy.getOFFLINE());
       System.out.printf("지역: %s\n", freeStudy.getArea());
-
     } else {
       System.out.printf("온/오프라인: %s\n", freeStudy.getONLINE());
     }
@@ -58,8 +57,6 @@ public class FreeStudyApplyDetailHandler implements Command {
     System.out.printf("설명: %s\n", freeStudy.getContent());
     System.out.printf("규칙: %s\n", freeStudy.getRule());
     System.out.printf("등록일: %s\n", freeStudy.getRegisteredDate());
-
-    freeStudy.setViewCount(freeStudy.getViewCount() + 1);
     System.out.printf("조회수: %d\n", freeStudy.getViewCount());
     System.out.printf("좋아요: %d\n", freeStudy.getLikeMembers().size());
     System.out.println();
