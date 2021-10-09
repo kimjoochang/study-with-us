c74d74cef3596df3f926382c31b200416e0a15d6
jei-kim <jeje2kim@gmail.com>
2021-10-09 17:45:32 +0900

    Update ServerAppJJ

--- project/study-with-us-project/app-server/src/main/java/com/studywithus/ServerAppJJ.java
+++ project/study-with-us-project/app-server/src/main/java/com/studywithus/ServerAppJJ.java
@@ -2,14 +2,12 @@ package com.studywithus;
 
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Collection;
 import java.util.HashMap;
 import com.studywithus.server.DataProcessor;
 import com.studywithus.server.RequestProcessor;
 import com.studywithus.table.ChargeStudyTableJJ;
 import com.studywithus.table.ExamScheduleTable;
 import com.studywithus.table.JobsScheduleTable;
-import com.studywithus.table.JsonDataTable;
 import com.studywithus.table.MemberTableJJ;
 import com.studywithus.table.MentorApplicationTable;
 import com.studywithus.table.PaymentTable;
@@ -17,11 +15,9 @@ import com.studywithus.table.PaymentTable;
 public class ServerAppJJ {
 
   public static void main(String[] args) throws Exception {
+
     System.out.println("서버 실행중");
     ServerSocket serverSocket = new ServerSocket(8888);
-
-    Socket socket = serverSocket.accept();
-    System.out.println("클라이언트가 접속했음");
 
     // RequestProcessor 가 사용할 DataProcessor 맵 준비
     HashMap<String,DataProcessor> dataProcessorMap = new HashMap<String,DataProcessor>();
@@ -30,31 +26,26 @@ public class ServerAppJJ {
 
     dataProcessorMap.put("member.", new MemberTableJJ());
     dataProcessorMap.put("chargeStudy.", new ChargeStudyTableJJ());
-    // [10.03, 04 추가]
     dataProcessorMap.put("payment.", new PaymentTable());
     dataProcessorMap.put("examScheduleTable.", new ExamScheduleTable());
     dataProcessorMap.put("jobsScheduleTable.", new JobsScheduleTable());
-
     dataProcessorMap.put("mentorApplication.", new MentorApplicationTable());
     //    dataProcessorMap.put("mentorApplication.", new MentorApplicationTable());
     //    dataProcessorMap.put("project.", new ProjectTable());
 
-    RequestProcessor requestProcessor = new RequestProcessor(socket, dataProcessorMap);
-    requestProcessor.service();
-    requestProcessor.close();
+    while (true) {
+      Socket socket = serverSocket.accept();
+      System.out.println("클라이언트 접속");
 
+      // 1) 새 실행 흐름 생성
+      RequestProcessor requestProcessor = new RequestProcessor(socket, dataProcessorMap);
 
-    // => 데이터를 파일에 저장한다.
-    Collection<DataProcessor> dataProcessors = dataProcessorMap.values();
-    for (DataProcessor dataProcessor : dataProcessors) {
-      if (dataProcessor instanceof JsonDataTable) {
-        // 만약 데이터 처리 담당자가 JsonDataTable 의 자손이라면,
-        ((JsonDataTable<?>)dataProcessor).save();
-      }
+      // 2) 새로 생성한 실행 흐름을 시작시킨다.
+      // => run()이 호출될 것이다.
+      // => 시작시킨 후 즉시 리턴한다. 
+      //    즉 새로 생성한 실행 흐름이 종료될 때까지 기다리지 않는다.
+      requestProcessor.start();
     }
-
-    System.out.println("서버 종료");
-    serverSocket.close();
   }
 }
 
