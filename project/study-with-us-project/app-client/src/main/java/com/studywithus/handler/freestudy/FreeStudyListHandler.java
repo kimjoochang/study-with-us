ee33f1a22e298cdb661fc372bbccbe0f3e12b035
SeonyoungHa <syh5004@naver.com>
2021-10-09 15:55:03 +0900

    Update FreeStudy/List,Search,UpdateHandler, CommunityListHandler

--- project/study-with-us-project/app-client/src/main/java/com/studywithus/handler/freestudy/FreeStudyListHandler.java
+++ project/study-with-us-project/app-client/src/main/java/com/studywithus/handler/freestudy/FreeStudyListHandler.java
@@ -2,36 +2,34 @@ package com.studywithus.handler.freestudy;
 
 import java.util.Collection;
 
+import com.studywithus.dao.FreeStudyDao;
 import com.studywithus.domain.Study;
 import com.studywithus.handler.Command;
 import com.studywithus.handler.CommandRequest;
-import com.studywithus.request.RequestAgent;
 
 public class FreeStudyListHandler implements Command {
 
-	RequestAgent requestAgent;
+	FreeStudyDao freeStudyDao;
 
-	public FreeStudyListHandler(RequestAgent requestAgent) {
-		this.requestAgent = requestAgent;
+	public FreeStudyListHandler(FreeStudyDao freeStudyDao) {
+		this.freeStudyDao = freeStudyDao;
 	}
 
 	@Override
 	public void execute(CommandRequest request) throws Exception {
 		System.out.println("[무료 스터디 / 조회]\n");
 
-		requestAgent.request("freeStudy.selectList", null);
+		Collection<Study> studyList = freeStudyDao.findAll();
 
-		// if (studyList.isEmpty()) {
-		// System.out.println("무료 스터디 게시글이 존재하지 않습니다.");
-		// return;
-		// }
-
-		if (requestAgent.getStatus().equals(RequestAgent.FAIL)) {
-			System.out.println("목록 조회 실패!");
+		if (studyList == null) {
+			System.out.println("무료 스터디 게시글이 존재하지 않습니다.");
 			return;
 		}
 
-		Collection<Study> studyList = requestAgent.getObjects(Study.class);
+		//		if (freeStudyDao.getStatus().equals(RequestAgent.FAIL)) {
+		//			System.out.println("목록 조회 실패!");
+		//			return;
+		//		}
 
 		for (Study freeStudy : studyList) {
 			if (freeStudy.getOnOffLine() == 1) {
