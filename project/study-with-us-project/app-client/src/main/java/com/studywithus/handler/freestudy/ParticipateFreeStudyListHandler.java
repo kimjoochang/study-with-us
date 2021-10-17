1a19d2f70f1cd3a44d99fa2930c8c71e29c5e0eb
Gyeryeong Ban <gyeryeong.ban@outlook.com>
2021-10-17 21:30:10 +0900

    Debug ParticipateFreeStudyList

--- project/study-with-us-project/app-client/src/main/java/com/studywithus/handler/freestudy/ParticipateFreeStudyListHandler.java
+++ project/study-with-us-project/app-client/src/main/java/com/studywithus/handler/freestudy/ParticipateFreeStudyListHandler.java
@@ -2,9 +2,11 @@ package com.studywithus.handler.freestudy;
 
 import java.util.Collection;
 import com.studywithus.dao.FreeStudyDao;
+import com.studywithus.domain.Member;
 import com.studywithus.domain.Study;
 import com.studywithus.handler.Command;
 import com.studywithus.handler.CommandRequest;
+import com.studywithus.handler.user.AuthLogInHandler;
 
 public class ParticipateFreeStudyListHandler implements Command {
 
@@ -24,89 +26,116 @@ public class ParticipateFreeStudyListHandler implements Command {
     int count = 0;
 
     for (Study freeStudy : freeStudyList) {
-      if (freeStudy.getOnOffLine() == 1) {
-        System.out.printf(
-            "[번호 = %d, 제목 = %s, 팀장 = %s, 온/오프라인 = %s, 모집인원 = %d / %d, 등록일 = %s, 조회수 = %d, 좋아요 = %d]\n",
-            freeStudy.getNo(), freeStudy.getTitle(), freeStudy.getWriter().getName(),
-            freeStudy.getONLINE(), freeStudy.getMembers().size(), freeStudy.getMaxMembers(),
-            freeStudy.getRegisteredDate(), freeStudy.getViewCount(),
-            freeStudy.getLikeMembers().size());
+      if (freeStudy.getParticipants().isEmpty()) {
+        if (count == 1) {
+          continue;
 
-      } else {
-        System.out.printf(
-            "[번호 = %d, 제목 = %s, 팀장 = %s, 온/오프라인 = %s, 지역 = %s, 모집인원 = %d / %d, 등록일 = %s, 조회수 = %d, 좋아요 = %d]\n",
-            freeStudy.getNo(), freeStudy.getTitle(), freeStudy.getWriter().getName(),
-            freeStudy.getOFFLINE(), freeStudy.getArea(), freeStudy.getMembers().size(),
-            freeStudy.getMaxMembers(), freeStudy.getRegisteredDate(), freeStudy.getViewCount(),
-            freeStudy.getLikeMembers().size());
+        } else {
+          count = 0;
+          continue;
+        }
+      }
+
+      for (Member participant : freeStudy.getParticipants()) {
+        if (participant.getNo() == AuthLogInHandler.getLoginUser().getNo()) {
+          count = 1;
+
+          if (freeStudy.getOnOffLine() == 1) {
+            System.out.printf(
+                "[번호 = %d, 제목 = %s, 팀장 = %s, 온/오프라인 = %s, 모집인원 = %d / %d, 등록일 = %s, 조회수 = %d, 좋아요 = %d]\n",
+                freeStudy.getNo(), freeStudy.getTitle(), freeStudy.getWriter().getName(),
+                freeStudy.getONLINE(), freeStudy.getMembers().size(), freeStudy.getMaxMembers(),
+                freeStudy.getRegisteredDate(), freeStudy.getViewCount(),
+                freeStudy.getLikeMembers().size());
+
+          } else {
+            System.out.printf(
+                "[번호 = %d, 제목 = %s, 팀장 = %s, 온/오프라인 = %s, 지역 = %s, 모집인원 = %d / %d, 등록일 = %s, 조회수 = %d, 좋아요 = %d]\n",
+                freeStudy.getNo(), freeStudy.getTitle(), freeStudy.getWriter().getName(),
+                freeStudy.getOFFLINE(), freeStudy.getArea(), freeStudy.getMembers().size(),
+                freeStudy.getMaxMembers(), freeStudy.getRegisteredDate(), freeStudy.getViewCount(),
+                freeStudy.getLikeMembers().size());
+          }
+
+        } else {
+          if (count == 1) {
+            continue;
+
+          } else {
+            count = 0;
+            continue;
+          }
+        }
       }
     }
 
     if (count == 0) {
-      System.out.println();
-      System.out.println("참여 무료 스터디가 존재하지 않습니다.");
-      return;
+      // System.out.println();
+      System.out.println("참여하신 무료 스터디가 존재하지 않습니다.");
+      // return;
     }
-
-    // if (freeStudyList.isEmpty()) {
-    // System.out.println("참여 무료 스터디가 존재하지 않습니다.");
-    // return;
-    // }
-
-    // Date nowDate = new Date(System.currentTimeMillis());
-
-    // 모집 인원 = 참여 인원 && 현재 시간 < 시작일
-    // for (Study study : LoginIdList) {
-    // for (int i = 0; i < LoginIdList.size(); i++) {
-    // if (LoginIdList.get(i).getMembers().size() == LoginIdList.get(i).getMaxMembers()
-    // && LoginIdList.get(i).getStartDate().compareTo(nowDate) == 1) {
-    // System.out.println("<<모집 완료>>");
-    // list(LoginIdList);
-    // System.out.println();
-    // }
-    // }
-    //
-    // 현재 시간 < 시작일
-    // for (int i = 0; i < LoginIdList.size(); i++) {
-    // if (LoginIdList.get(i).getStartDate().compareTo(nowDate) == 1) {
-    // System.out.println("<<모집 중>>");
-    // list(LoginIdList);
-    // System.out.println();
-    // }
-    // }
-    //
-    // 현재 시간 >= 시작일
-    // for (int i = 0; i < LoginIdList.size(); i++) {
-    // if (LoginIdList.get(i).getStartDate().compareTo(nowDate) != 1) {
-    // System.out.println("<<진행 중>>");
-    // list(LoginIdList);
-    // System.out.println();
-    // }
-    // }
-    // }
-
-    // 내가 참여한 무료 스터디 리스트 출력
-    // private void list(List<Study> LoginIdList) {
-    // for (Study freeStudy : LoginIdList) {
-    // if (freeStudy.getOnOffLine() == 1) {
-    // System.out.printf(
-    // "[번호 = %d, 제목 = %s, 팀장 = %s, 온/오프라인 = %s, 모집인원 = %d / %d, 등록일 = %s, 조회수 = %d, 좋아요 = %d]\n",
-    // freeStudy.getNo(), freeStudy.getTitle(), freeStudy.getWriter().getName(),
-    // freeStudy.getONLINE(), freeStudy.getMembers().size(), freeStudy.getMaxMembers(),
-    // freeStudy.getRegisteredDate(), freeStudy.getViewCount(),
-    // freeStudy.getLikeMembers().size());
-    // } else {
-    // System.out.printf(
-    // "[번호 = %d, 제목 = %s, 팀장 = %s, 온/오프라인 = %s, 지역 = %s, 모집인원 = %d / %d, 등록일 = %s, 조회수 = %d, 좋아요 =
-    // %d]\n",
-    // freeStudy.getNo(), freeStudy.getTitle(), freeStudy.getWriter().getName(),
-    // freeStudy.getOFFLINE(), freeStudy.getArea(), freeStudy.getMembers().size(),
-    // freeStudy.getMaxMembers(), freeStudy.getRegisteredDate(), freeStudy.getViewCount(),
-    // freeStudy.getLikeMembers().size());
-    // }
-    // }
   }
 }
+
+// if (freeStudyList.isEmpty()) {
+// System.out.println("참여 무료 스터디가 존재하지 않습니다.");
+// return;
+// }
+
+// Date nowDate = new Date(System.currentTimeMillis());
+
+// 모집 인원 = 참여 인원 && 현재 시간 < 시작일
+// for (Study study : LoginIdList) {
+// for (int i = 0; i < LoginIdList.size(); i++) {
+// if (LoginIdList.get(i).getMembers().size() == LoginIdList.get(i).getMaxMembers()
+// && LoginIdList.get(i).getStartDate().compareTo(nowDate) == 1) {
+// System.out.println("<<모집 완료>>");
+// list(LoginIdList);
+// System.out.println();
+// }
+// }
+//
+// 현재 시간 < 시작일
+// for (int i = 0; i < LoginIdList.size(); i++) {
+// if (LoginIdList.get(i).getStartDate().compareTo(nowDate) == 1) {
+// System.out.println("<<모집 중>>");
+// list(LoginIdList);
+// System.out.println();
+// }
+// }
+//
+// 현재 시간 >= 시작일
+// for (int i = 0; i < LoginIdList.size(); i++) {
+// if (LoginIdList.get(i).getStartDate().compareTo(nowDate) != 1) {
+// System.out.println("<<진행 중>>");
+// list(LoginIdList);
+// System.out.println();
+// }
+// }
+// }
+
+// 내가 참여한 무료 스터디 리스트 출력
+// private void list(List<Study> LoginIdList) {
+// for (Study freeStudy : LoginIdList) {
+// if (freeStudy.getOnOffLine() == 1) {
+// System.out.printf(
+// "[번호 = %d, 제목 = %s, 팀장 = %s, 온/오프라인 = %s, 모집인원 = %d / %d, 등록일 = %s, 조회수 = %d, 좋아요 = %d]\n",
+// freeStudy.getNo(), freeStudy.getTitle(), freeStudy.getWriter().getName(),
+// freeStudy.getONLINE(), freeStudy.getMembers().size(), freeStudy.getMaxMembers(),
+// freeStudy.getRegisteredDate(), freeStudy.getViewCount(),
+// freeStudy.getLikeMembers().size());
+// } else {
+// System.out.printf(
+// "[번호 = %d, 제목 = %s, 팀장 = %s, 온/오프라인 = %s, 지역 = %s, 모집인원 = %d / %d, 등록일 = %s, 조회수 = %d, 좋아요 =
+// %d]\n",
+// freeStudy.getNo(), freeStudy.getTitle(), freeStudy.getWriter().getName(),
+// freeStudy.getOFFLINE(), freeStudy.getArea(), freeStudy.getMembers().size(),
+// freeStudy.getMaxMembers(), freeStudy.getRegisteredDate(), freeStudy.getViewCount(),
+// freeStudy.getLikeMembers().size());
+// }
+// }
+// }
+// }
 
 // ParticipateFreeStudyListHandler Ver.1
 // for (int i = 0; i < myParticipatedFreeStudyList.size(); i++) {
