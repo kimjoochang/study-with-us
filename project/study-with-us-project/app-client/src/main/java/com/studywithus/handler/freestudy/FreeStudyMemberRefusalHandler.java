35f26c449bd195b60d54443583b8ac9dc41ff57d
Gyeryeong Ban <gyeryeong.ban@outlook.com>
2021-10-18 19:34:11 +0900

    Update FreeStudyMemberRefusal

--- project/study-with-us-project/app-client/src/main/java/com/studywithus/handler/freestudy/FreeStudyMemberRefusalHandler.java
+++ project/study-with-us-project/app-client/src/main/java/com/studywithus/handler/freestudy/FreeStudyMemberRefusalHandler.java
@@ -9,50 +9,53 @@ import com.studywithus.util.Prompt;
 
 public class FreeStudyMemberRefusalHandler implements Command {
 
-	FreeStudyDao freeStudyDao;
+  FreeStudyDao freeStudyDao;
 
-	public FreeStudyMemberRefusalHandler(FreeStudyDao freeStudyDao) {
-		this.freeStudyDao = freeStudyDao;
-	}
+  public FreeStudyMemberRefusalHandler(FreeStudyDao freeStudyDao) {
+    this.freeStudyDao = freeStudyDao;
+  }
 
-	@Override
-	public void execute(CommandRequest request) throws Exception {
-		System.out.println("[마이 페이지 / 나의 활동 / 나의 스터디 / 내가 생성한 무료 스터디/ 상세보기 / 승인 거절]\n");
-		int no = (int) request.getAttribute("freeNo");
-		Study freeStudy = freeStudyDao.findByNo(no);
+  @Override
+  public void execute(CommandRequest request) throws Exception {
+    System.out.println("[마이 페이지 / 나의 활동 / 나의 스터디 / 내가 생성한 무료 스터디/ 상세보기 / 승인 거절]\n");
+    int no = (int) request.getAttribute("freeNo");
+    Study freeStudy = freeStudyDao.findByNo(no);
 
-		if (freeStudy == null) {
-			System.out.println("해당 번호의 게시글이 없습니다.");
-			return;
-		}
+    if (freeStudy == null) {
+      System.out.println("해당 번호의 게시글이 없습니다.");
+      return;
+    }
 
-		while (true) {
-			for (Member freeApplicant : freeStudy.getApplicants()) {
-				System.out.printf("[번호 = %d, 이름 = %s]", freeApplicant.getNo(), freeApplicant.getName());
-			}
-			int applicantNo = Prompt.inputInt("신청자 번호를 입력하세요. > ");
-			String input = Prompt.inputString("해당 회원을 승인 거절하시겠습니까? (y/N) ");
+    while (true) {
+      for (Member freeApplicant : freeStudy.getApplicants()) {
+        System.out.printf("[번호 = %d, 이름 = %s]", freeApplicant.getNo(), freeApplicant.getName());
+      }
+      int applicantNo = Prompt.inputInt("신청자 번호를 입력하세요. > ");
+      String input = Prompt.inputString("해당 회원을 승인 거절하시겠습니까? (y/N) ");
 
-			if (input.equalsIgnoreCase("n") || input.length() == 0) {
-				System.out.println();
-				System.out.println("멤버 승인 거절이 취소되었습니다.");
-				return;
+      if (input.equalsIgnoreCase("n") || input.length() == 0) {
+        System.out.println();
+        System.out.println("멤버 승인 거절이 취소되었습니다.");
+        return;
 
-			} else if (input.equalsIgnoreCase("y")) {
-				for (int i = 0; i < freeStudy.getParticipants().size(); i++) {		
-					if(applicantNo == freeStudy.getApplicants().get(i).getNo()) 
-						freeStudy.getApplicants().remove(i);
-					freeStudyDao.update(freeStudy);
-					System.out.println();
-					System.out.println("멤버 승인 거절이 완료되었습니다.");
-					return;
-				}
-			} else {
-				System.out.println();
-				System.out.println("다시 입력하세요.\n");
-				continue;
-			}
-		}
-		// System.out.println("무료스터디 멤버 승인 거절이 완료되었습니다.");
-	}
+      } else if (input.equalsIgnoreCase("y")) {
+        for (int i = 0; i < freeStudy.getApplicants().size(); i++) {
+          if (applicantNo == freeStudy.getApplicants().get(i).getNo())
+            freeStudy.getApplicants().remove(i);
+        }
+
+        freeStudyDao.update(freeStudy);
+
+        System.out.println();
+        System.out.println("멤버 승인 거절이 완료되었습니다.");
+        return;
+
+      } else {
+        System.out.println();
+        System.out.println("다시 입력하세요.\n");
+        continue;
+      }
+    }
+    // System.out.println("무료스터디 멤버 승인 거절이 완료되었습니다.");
+  }
 }
