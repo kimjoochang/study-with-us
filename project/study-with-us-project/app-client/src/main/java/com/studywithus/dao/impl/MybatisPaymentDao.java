02c959fbe70c3a8367419ac2b3a6363afcea72e8
jei-kim <jeje2kim@gmail.com>
2021-10-23 10:22:24 +0900

    Create MybatisPaymentDao

---
+++ project/study-with-us-project/app-client/src/main/java/com/studywithus/dao/impl/MybatisPaymentDao.java
@@ -0,0 +1,43 @@
+package com.studywithus.dao.impl;
+
+import java.util.HashMap;
+import java.util.List;
+import org.apache.ibatis.session.SqlSession;
+import com.studywithus.dao.PaymentDao;
+import com.studywithus.domain.Payment;
+
+public class MybatisPaymentDao implements PaymentDao {
+
+  SqlSession sqlSession;
+
+  public MybatisPaymentDao(SqlSession sqlSession) {
+    this.sqlSession = sqlSession;
+  }
+
+  @Override
+  public void insert(Payment payment) throws Exception {
+    sqlSession.insert("PaymentMapper.insert", payment);
+    sqlSession.commit();
+  }
+
+  @Override
+  public List<Payment> findAll() throws Exception {
+    return sqlSession.selectList("PaymentMapper.findAll");
+  }
+
+  @Override
+  public Payment findByNo(int studyNo, int memberNo) throws Exception {
+    HashMap<Object,Obeject> params = new HashMap<>();
+    params.put("studyNo", String.valueOf(studyNo));
+    params.put("memberNo", String.valueOf(memberNo));
+
+    return sqlSession.selectOne("PaymentMapper.findByEmailAndPassword", params);
+  }
+
+  @Override
+  public void update(Payment payment) throws Exception {
+    sqlSession.update("PaymentMapper.update", payment);
+    sqlSession.commit();
+  }
+
+}
