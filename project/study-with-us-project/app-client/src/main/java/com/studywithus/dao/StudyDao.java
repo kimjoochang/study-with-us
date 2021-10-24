0d5d0d02ce36a0163c78230298e4e9457465edcc
kimjoochang <kimjc0707@naver.com>
2021-10-24 20:29:34 +0900

    Update StudyDaoClass & XML

--- project/study-with-us-project/app-client/src/main/java/com/studywithus/dao/StudyDao.java
+++ project/study-with-us-project/app-client/src/main/java/com/studywithus/dao/StudyDao.java
@@ -12,20 +12,22 @@ public interface StudyDao {
   void insert(Study study) throws Exception;
   void update(Study chargeStudy) throws Exception;
 
-  List<Study> findAllRegisterStudy(int no) throws Exception;
-  Study findByNoRegisterStudy(@Param("writerNo") int writerNo, @Param("studyNo") int studyNo) throws Exception;
-
-  List<Study> findAllParticipateStudy(int no) throws Exception;
-  Study findByNoParticipateStudy(@Param("memberNo") int writerNo, @Param("studyNo") int studyNo) throws Exception;
-
   void insertInterest(@Param("memberNo") int memberNo, @Param("studyNo") int studyNo) throws Exception;
   List<Study> findAllInterest(int no) throws Exception;
   Study findByNoInterest(@Param("memberNo") int memberNo, @Param("studyNo") int studyNo) throws Exception;
 
+  List<Study> findAllRegisterStudy(int no) throws Exception;
+  Study findByNoRegisterStudy(@Param("writerNo") int writerNo, @Param("studyNo") int studyNo) throws Exception;
+
+  List<Study> findAllParticipateStudy(@Param("memberNo") int memberNo, @Param("myStatus") int status) throws Exception;
+  Study findByNoParticipateStudy(@Param("memberNo") int writerNo, @Param("studyNo") int studyNo, @Param("myStatus") int status) throws Exception;
+
   // 0 = 무료 신청, 1 = 무료 참여자, 2 = 유료 참여자(결제한 사람)
-  void insertStudyMember(@Param("memberNo") int memberNo, @Param("studyNo") int studyNo, @Param("status") int status) throws Exception;
+  void insertStudyMember(@Param("memberNo") int memberNo, @Param("studyNo") int studyNo, @Param("myStatus") int status) throws Exception;
   List<Study> findAllApplyStudy(int no) throws Exception;
   Study findByNoApplyStudy(@Param("memberNo") int memberNo, @Param("studyNo") int studyNo) throws Exception;
+  void updateMyStatus(@Param("memberNo") int memberNo, @Param("studyNo") int studyNo, @Param("myStatus") int status) throws Exception;
+
 
   void delete(int no) throws Exception;
   void deleteStudyMember(@Param("memberNo") int memberNo, @Param("studyNo") int studyNo) throws Exception;
