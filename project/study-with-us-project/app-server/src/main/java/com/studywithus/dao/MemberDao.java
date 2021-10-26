3a0c11cf231abc067ecbff295617e44384cc5ba9
jei-kim <jeje2kim@gmail.com>
2021-10-26 18:28:46 +0900

    211026 settings

---
+++ project/study-with-us-project/app-server/src/main/java/com/studywithus/dao/MemberDao.java
@@ -0,0 +1,19 @@
+package com.studywithus.dao;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import com.studywithus.domain.Member;
+
+public interface MemberDao {
+  List<Member> findAll() throws Exception;
+  Member findByNo(int no) throws Exception;
+  Member findByEmail(String email) throws Exception; // 회원가입 시 아이디 중복검사
+  Member findMemberByNamePhoneNumber(@Param("name") String name, @Param("phoneNumber") String phoneNumber) throws Exception; // 아이디 찾기
+  Member findMember(@Param("name") String name, @Param("email") String email, @Param("phoneNumber") String phoneNumber) throws Exception; // 비밀번호 재설정
+  Member findMemberByEmailPassword(@Param("email") String email, @Param("password") String password) throws Exception; // 로그인
+  void insert(Member member) throws Exception;
+  void update(Member member) throws Exception;
+  void updatePassword(Member member) throws Exception;
+  void delete(int no) throws Exception;
+
+}
