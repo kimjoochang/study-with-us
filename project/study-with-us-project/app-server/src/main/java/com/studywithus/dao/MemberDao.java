dbd6b55ff058ca903aa90498105200146ba98a7f
kimjoochang <kimjc0707@naver.com>
2021-10-27 10:35:49 +0900

    Add Package_User in Package_Servlet

--- project/study-with-us-project/app-server/src/main/java/com/studywithus/dao/MemberDao.java
+++ project/study-with-us-project/app-server/src/main/java/com/studywithus/dao/MemberDao.java
@@ -7,7 +7,7 @@ import com.studywithus.domain.Member;
 public interface MemberDao {
   List<Member> findAll() throws Exception;
   Member findByNo(int no) throws Exception;
-  Member findByEmail(String email) throws Exception; // 회원가입 시 아이디 중복검사
+  int findByEmail(String email) throws Exception; // 회원가입 시 아이디 중복검사
   Member findMemberByNamePhoneNumber(@Param("name") String name, @Param("phoneNumber") String phoneNumber) throws Exception; // 아이디 찾기
   Member findMember(@Param("name") String name, @Param("email") String email, @Param("phoneNumber") String phoneNumber) throws Exception; // 비밀번호 재설정
   Member findMemberByEmailPassword(@Param("email") String email, @Param("password") String password) throws Exception; // 로그인
