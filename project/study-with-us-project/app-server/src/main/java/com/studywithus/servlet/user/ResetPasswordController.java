ea83a43c59d3769cc4eed40d9329a7cf3a2a9a54
jei-kim <jeje2kim@gmail.com>
2021-11-03 20:27:01 +0900

    Udpate resetpwds

---
+++ project/study-with-us-project/app-server/src/main/java/com/studywithus/servlet/user/ResetPasswordController.java
@@ -0,0 +1,56 @@
+package com.studywithus.servlet.user;
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.ibatis.session.SqlSession;
+import com.studywithus.dao.MemberDao;
+import com.studywithus.domain.Member;
+
+@WebServlet("/user/resetpwd")
+public class ResetPasswordController extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+
+  SqlSession sqlSession;
+  MemberDao memberDao;
+
+  @Override
+  public void init(ServletConfig config) throws ServletException {
+    ServletContext servletContext = config.getServletContext();
+    sqlSession = (SqlSession) servletContext.getAttribute("sqlSession");
+    memberDao = (MemberDao) servletContext.getAttribute("memberDao");
+  }
+
+  @Override
+  protected void service(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+
+    try {
+      String name = request.getParameter("name");
+      String email = request.getParameter("email");
+      String phoneNumber = request.getParameter("phoneNumber");
+
+      Member member = memberDao.findMember(name, email, phoneNumber);
+
+      if (member == null) {
+        throw new Exception("해당 정보의 회원이 없습니다.");
+      } 
+
+      member.setPassword(request.getParameter("password"));
+
+      memberDao.update(member);
+      sqlSession.commit();
+
+      response.sendRedirect("/swu/index");
+
+    } catch (Exception e) {
+      request.setAttribute("error", e);
+      request.getRequestDispatcher("/Error.jsp").forward(request, response);
+
+    }
+  }
+}
--- project/study-with-us-project/app-server/src/main/webapp/user/ResetPasswordForm.jsp
+++ project/study-with-us-project/app-server/src/main/webapp/user/ResetPasswordForm.jsp
@@ -1,15 +1,25 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+    pageEncoding="UTF-8"
+    trimDirectiveWhitespaces="true" %>
 <!DOCTYPE html>
 <html>
 <head>
   <title>스터디 위더스</title>
+  <style>
+  label {
+    margin-right: 5px;
+    text-align: right;
+    display: inline-block;
+    width: 60px;
+  }
+  </style>
 </head>
 
 <body>
-<form method = "post" action="findemail">
-  <table border = "2"  align = "center">
-      <caption><h1>아이디 찾기</h1></caption>
+<form method="post" action='resetpwd'>
+<h1>비밀번호 변경 확인</h1>
+  <table border = "2  align = "center">
+      <caption><h1>비밀번호 변경 확인</h1></caption>
       <tr>
       <th>
         이름
@@ -18,6 +28,12 @@
         <input type = "text" name ="name" placeholder="홍길동" required>
       </td>
       <th>
+        이메일
+      </th>
+    <td>
+      <input type = "email" name ="email" placeholder="study@swu.com" required>
+    </td>
+    <th>
         휴대폰번호
       </th>
     <td>
@@ -36,6 +52,18 @@
   
 </div>
 </form>     
+    
+ <button>변경</button> <a href='/swu/index'>[취소]</a> 
+</form>
 
 </body>
 </html>
+
+
+
+
+
+
+
+
+
